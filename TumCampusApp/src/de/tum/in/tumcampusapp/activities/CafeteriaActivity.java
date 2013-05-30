15cf4f66f41f89d4c5b69b4809225ecd58367299
Sascha Moecker <mckeck88@gmx.de>
2013-05-30 11:57:52 +0200

    Addes Comment Sascha

--- TumCampusApp/src/de/tum/in/tumcampusapp/activities/CafeteriaActivity.java
+++ TumCampusApp/src/de/tum/in/tumcampusapp/activities/CafeteriaActivity.java
@@ -17,6 +17,7 @@ import de.tum.in.tumcampusapp.models.managers.CafeteriaManager;
  * Activity to show cafeterias and meals selected by date
  * @author TCA Team
  * hello world
+ * Sascha
  */
 public class CafeteriaActivity extends ActivityForDownloadingExternal implements OnItemClickListener {
 
