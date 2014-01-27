42941fe0524b0c387bab393a5cca963c44d20945
sepidehmesbah <Sepidemesbah@bitbucker.org>
2014-01-27 20:18:03 +0100

    Intelligent reordering of menu items

---
+++ TumCampusApp/src/de/tum/in/tumcampusapp/activities/ImplicitCounter.java
@@ -0,0 +1,72 @@
+package de.tum.in.tumcampusapp.activities;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.view.ViewPager;
+import android.widget.Toast;
+import de.tum.in.tumcampus.R;
+import de.tum.in.tumcampusapp.activities.generic.ActivityForDownloadingExternal;
+import de.tum.in.tumcampusapp.adapters.EventsSectionsPagerAdapter;
+import de.tum.in.tumcampusapp.auxiliary.Const;
+import de.tum.in.tumcampusapp.auxiliary.PersonalLayoutManager;
+
+/**
+ * Displays Events which are fetched from Facebook
+ * 
+ * @author Sascha Moecker
+ * 
+ */
+public class ImplicitCounter extends Activity{
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	
+	}
+	public static boolean Counter(String key,Context context)
+	{
+		boolean myboolean=true;
+		//Counting number of the times that the user used this activity.
+				 SharedPreferences sp = context.getSharedPreferences("MyPrefrence", Context.MODE_PRIVATE);
+				int myvalue = sp.getInt(key,0);
+				myvalue=myvalue+1;
+				SharedPreferences.Editor editor = sp.edit();
+				editor.putInt(key,myvalue);
+				editor.commit();
+				////
+
+				 int myIntValue = sp.getInt(key,0);
+				 if(myIntValue==5){
+					    SharedPreferences sharedPrefs =PreferenceManager.getDefaultSharedPreferences(context);
+						SharedPreferences.Editor editor1 = sharedPrefs.edit();
+						editor1.putBoolean(key, true);
+						editor1.commit();
+						editor.putInt(key,0);
+						editor.commit();
+
+				 }
+		return myboolean;
+				
+		
+	}
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		
+	}
+}
