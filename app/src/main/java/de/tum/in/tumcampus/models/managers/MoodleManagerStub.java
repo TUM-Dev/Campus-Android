a3df76f15a33dcd40c503ac20e22f49e3409ba59
arash-a2k <arash.khataei@gmail.com>
2015-06-07 21:25:20 +0200

    Stub for Moodle Manager updated

--- app/src/main/java/de/tum/in/tumcampus/models/managers/MoodleManagerStub.java
+++ app/src/main/java/de/tum/in/tumcampus/models/managers/MoodleManagerStub.java
@@ -1,6 +1,7 @@
 package de.tum.in.tumcampus.models.managers;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import de.tum.in.tumcampus.models.MoodleObject;
 import de.tum.in.tumcampus.models.MoodleUserCourses;
@@ -14,13 +15,13 @@ public class MoodleManagerStub {
     private String userName;
     private String password;
 
-    private final HashMap<String, String> USER_COURSES = new HashMap<String, String>(){{
+    private final Map<String, String> USER_COURSES = new HashMap<String, String>(){{
             put("Deutsch als Fremdsprache B1.2",""); put("Praktikum - Betriebssysteme - Google Android (IN0012, IN2106, IN4004)","Welcome to the Android Practical Summer Term Course  (SS15) !");
             put("Basic  Moodle and Mountaineering", "This course is for senior students planning an ascent of Mont Blanc in July. It is also designed to take Moodle newbies through a number of activities showing off the best of Moodle.");
             put("Psychology in Cinema ","In this course we study three films: Spider, A Beautiful Mind, and Fight Club. The main focus of the course will be the ways in which psychosis is represented in the films in terms of macro, plot, narrative structure and micro etc. We consider the wider cultural meaning and implication of films dealing with psychology" );
     }};
 
-    public HashMap<?,?> getCoursesList(){
+    public Map<?,?> getCoursesList(){
         return USER_COURSES;
     }
 
