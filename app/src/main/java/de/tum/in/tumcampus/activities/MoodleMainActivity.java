fe64b06d6d5d2b585acd1b9521b6dc15fb88da4c
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2015-06-07 12:54:56 +0200

    Fix to merge conflicts

--- app/src/main/java/de/tum/in/tumcampus/activities/MoodleMainActivity.java
+++ app/src/main/java/de/tum/in/tumcampus/activities/MoodleMainActivity.java
@@ -2,16 +2,12 @@ package de.tum.in.tumcampus.activities;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.AdapterView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 
 import de.tum.in.tumcampus.R;
-<<<<<<< HEAD
 import de.tum.in.tumcampus.activities.generic.ActivityForDownloadingExternal;
-import de.tum.in.tumcampus.auxiliary.Utils;
-=======
->>>>>>> 8a6fc1c6451eae0b2ded1f894da197d9d67fd047
 import de.tum.in.tumcampus.models.managers.MoodleManager;
 
 public class MoodleMainActivity extends ActivityForDownloadingExternal implements OnItemClickListener {
@@ -27,20 +23,12 @@ public class MoodleMainActivity extends ActivityForDownloadingExternal implement
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-   /* maybe used later. copied from news activity */
-            //requestDownload(false);
 
-            Utils.log("On Create is called!");
-            moodleManager = new MoodleManager();
-            moodleManager.requestUserToken("student", "moodle", this);
-            Utils.log(moodleManager.getMoodleCourses(this));
-=======
         setContentView(R.layout.activity_moodle_main);
         moodleManager = new MoodleManager();
         //moodleManager.requestUserToken("student","moodle",this);
         moodleManager.requestServiceCall(this, "username=student&password=moodle&service=moodle_mobile_app");
->>>>>>> 8a6fc1c6451eae0b2ded1f894da197d9d67fd047
+
     }
 
 
