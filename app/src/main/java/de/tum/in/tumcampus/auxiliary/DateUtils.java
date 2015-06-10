ee7e8df1f92e8331564dbfa8837b3730eb59f925
arash-a2k <arash.khataei@gmail.com>
2015-06-10 16:18:55 +0200

    added method     public static Date parseSimpleDateFormat(String datetime) updated method epochToDate

--- app/src/main/java/de/tum/in/tumcampus/auxiliary/DateUtils.java
+++ app/src/main/java/de/tum/in/tumcampus/auxiliary/DateUtils.java
@@ -20,6 +20,7 @@ public class DateUtils {
 
     private static final String formatSQL = "yyyy-MM-dd HH:mm:ss"; // 2014-06-30 16:31:57
     private static final String formatISO = "yyyy-MM-dd'T'HH:mm:ss'Z'"; // 2014-06-30T16:31:57.878Z
+    private static final String SIMPLE_DATE_FORMAT = "EEE MMM dd HH:mm:ss zzz yyyy";
     private static final String logTag = "DateUtils";
 
     public static String getRelativeTimeISO(String timestamp, Context context) {
@@ -92,6 +93,20 @@ public class DateUtils {
         return null;
     }
 
+
+    public static Date parseSimpleDateFormat(String datetime) {
+        if(datetime == null) {
+            return null;
+        }
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat(SIMPLE_DATE_FORMAT, Locale.ENGLISH);
+            return formatter.parse(datetime);
+        } catch (ParseException e) {
+            Log.e(logTag, "Parsing SIMPLE_DATE_FORMAT date failed");
+        }
+        return null;
+    }
+
     private static Time getCurrentTime() {
         Time now = new Time();
         now.setToNow();
@@ -103,9 +118,13 @@ public class DateUtils {
          * gets the epochTime in seconds
          * returns the corresponding GeorgianCalendar
          */
+        if(epochTime == null) {
+            return null;
+        }
         GregorianCalendar c = new GregorianCalendar();
         //  This method gets the epochTime in milliseconds
         c.setTimeInMillis(epochTime * 1000);
         return c;
     }
+
 }
