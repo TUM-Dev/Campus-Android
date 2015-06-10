96ea99d5d8b848c89a27298c53c5070556b59de9
arash-a2k <arash.khataei@gmail.com>
2015-06-10 16:20:05 +0200

    added to methods in order to return String value regarding this event, and getting the corresponding Date out of a string created by Event class

--- app/src/main/java/de/tum/in/tumcampus/models/MoodleEvent.java
+++ app/src/main/java/de/tum/in/tumcampus/models/MoodleEvent.java
@@ -1,9 +1,11 @@
 
 package de.tum.in.tumcampus.models;
 
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import de.tum.in.tumcampus.auxiliary.DateUtils;
+import de.tum.in.tumcampus.auxiliary.Utils;
 
 
 /**
@@ -43,6 +45,43 @@ public class MoodleEvent {
     }
 
     public String getDateString(){
-        return date.getTime().toString();
+        String time = date.getTime().toString();
+        int minutes = duration / 60;
+        String duration = "duration: " + String.valueOf(minutes) + " min ";
+        return date.getTime().toString() + "\n" + duration;
+    }
+
+    public static int getDuration(String eventDateString){
+        /**
+         * gets a DateString created by event class and returns
+         * an integer value for seconds of duration inside the String
+         * @param eventDateString String created by an event object
+         * @return an int, representing the amount of duration in seconds
+         */
+        String [] values = eventDateString.split("\n");
+        String duration = values[1];
+        duration = duration.replaceAll("[^0-9]","");
+        return Integer.valueOf(duration) * 60;
+    }
+
+    public static GregorianCalendar getDate(String eventDateString){
+        /**
+         * gets a DateString created by event class and returns
+         * an GregorianCalendar for date inside the String
+         * @param eventDateString String created by an event object
+         * @return GregorianCalendar
+         */
+        String [] values = eventDateString.split("\n");
+        String dateString = values[0];
+        Date date = DateUtils.parseSimpleDateFormat(dateString);
+        if (date!=null) {
+            GregorianCalendar g = new GregorianCalendar();
+            g.setTime(date);
+            return g;
+        }else {
+            Utils.log("#error: failed to convert to GregorianCalendar: "  + eventDateString);
+            return null;
+        }
+
     }
 }
