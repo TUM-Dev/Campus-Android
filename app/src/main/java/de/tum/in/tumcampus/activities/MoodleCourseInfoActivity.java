f31a9d45977ffd47439f65f745d93f38b203d0bb
arash-a2k <arash.khataei@gmail.com>
2015-06-14 12:13:18 +0200

    initial commit. MoodleCourseInfo activity

---
+++ app/src/main/java/de/tum/in/tumcampus/activities/MoodleCourseInfoActivity.java
@@ -0,0 +1,225 @@
+package de.tum.in.tumcampus.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.net.URL;
+import java.util.List;
+
+import de.tum.in.tumcampus.R;
+import de.tum.in.tumcampus.activities.generic.ActivityForDownloadingExternal;
+import de.tum.in.tumcampus.adapters.MoodleCourseInfoExpandableAdapter;
+import de.tum.in.tumcampus.auxiliary.Utils;
+import de.tum.in.tumcampus.models.MoodleCourseContent;
+import de.tum.in.tumcampus.models.MoodleCourseModule;
+import de.tum.in.tumcampus.models.MoodleCourseSection;
+import de.tum.in.tumcampus.models.managers.MockMoodleManager;
+import de.tum.in.tumcampus.models.managers.MoodleManager;
+import de.tum.in.tumcampus.models.managers.MoodleUpdateListViewDelegate;
+import de.tum.in.tumcampus.models.managers.RealMoodleManager;
+
+/**
+ * Created by a2k on 6/10/2015.
+ * This activity shows the contents of a course page in moodle. The user can choose the contents, which he
+ * is interested in, and they if they are files, they will be downloaded, otherwise a browser will be opened
+ * to show the corresponding URL.
+ */
+public class MoodleCourseInfoActivity extends ActivityForDownloadingExternal implements AdapterView.OnItemClickListener, MoodleUpdateListViewDelegate, ExpandableListView.OnChildClickListener, ExpandableListView.OnGroupClickListener {
+
+
+    MoodleManager realManager;
+    MoodleManager mockManager;
+    private Intent intent;
+    private String userToken, courseName;
+    private MoodleCourseInfoExpandableAdapter dataAdapter;
+    private List<MoodleCourseSection> sections;
+    private ExpandableListView view_course_sections;
+    private TextView title;
+
+    public MoodleCourseInfoActivity() {super("MoodleCourseInfo", R.layout.activity_moodle_course_info);}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        intent = getIntent();
+        baseSetup();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_moodle, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.moodle_my_courses:
+                Intent coursesIntent = new Intent(this,MoodleMainActivity.class);
+                coursesIntent.putExtra("user_token", realManager.getToken());
+                startActivity(coursesIntent);
+                return true;
+            case R.id.events:
+                Intent eventIntent = new Intent(this,MoodleEventsActivity.class);
+                eventIntent.putExtra("user_token", realManager.getToken());
+                startActivity(eventIntent);
+                return true;
+
+            case R.id.moodle_profile:
+                //TODO add code to start profile activity
+                return true;
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void refreshListView() {
+        //TODO @carlo do whatever needs to be done
+    }
+
+    /**
+     * handles the click events on the child items in the expandable list
+     * @param parent
+     * @param v
+     * @param groupPosition
+     * @param childPosition
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+        try {
+            MoodleCourseContent content = (MoodleCourseContent) dataAdapter.getChild(groupPosition, childPosition);
+            if (content != null) {
+                URL fileURL = content.getFileurl();
+                if (fileURL != null){
+
+                    //TODO think about modifying the URL to have the token or userid
+                    Utils.log(String.format("Got this URL %s", fileURL.toString()));
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(fileURL.toString()));
+                    startActivity(browserIntent);
+                    return true;
+                }else {
+                    Utils.showToast(this.getApplicationContext(), R.string.no_url);
+                    return true;
+                }
+            } else
+                Utils.log(String.format("No Content found for group posiiton %d and childPosition %d", groupPosition, childPosition));
+            return false;
+        }catch (Exception e) {
+            Utils.log(e);
+            return false;
+        }
+    }
+
+    /**
+     * handles the event on clicks on the group items of the expandable list. If it is a section header,
+     * does nothing. If it is a module and it has children, expand the group. if it does not have children
+     * it means that it has an URL associated with it. Open this URL in a new browser activity.
+     * @param parent
+     * @param v
+     * @param groupPosition
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+        try {
+
+            if (dataAdapter.getChildrenCount(groupPosition) == 0) {
+                Object header = dataAdapter.getGroup(groupPosition);
+
+                // do nothing for a course section
+                if (header instanceof MoodleCourseSection)
+                    return true;
+                else {
+                    URL url = ((MoodleCourseModule) header).getUrl();
+                    if (url != null) {
+
+                        //TODO think about modifying the URL to have the token or userid
+                        Utils.log(String.format("Got this URL %s", url.toString()));
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url.toString()));
+                        startActivity(browserIntent);
+                        return true;
+                    } else {
+                        Utils.showToast(this.getApplicationContext(), R.string.no_url);
+                        return true;
+                    }
+                }
+            } else
+                // if this group view has childs just expand or hide no URL is available
+                if (parent.isGroupExpanded(groupPosition) == false) {
+
+                    //  API Level 14+ allows you to animate the Group expansion...
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+                        parent.expandGroup(groupPosition, true);
+
+                        //  else just expand the Group without animation
+                    else
+                        parent.expandGroup(groupPosition);
+                } else
+                    //  collapse the ListView Group/s
+                    parent.collapseGroup(groupPosition);
+            return true;
+        }catch(Exception e){
+            Utils.log(e);
+            return false;
+        }
+    }
+
+    private void baseSetup(){
+        try {
+            // getting info from intent
+            userToken = intent.getStringExtra("user_token");
+            courseName = intent.getStringExtra("course_name");
+            courseName = (courseName!=null) ? courseName : getString(R.string.moodle_course_name_not_found);
+            int courseId = intent.getIntExtra("course_id", 0);
+            if (courseId == 0)
+                Utils.log(String.format("Warning! course id is 0=defaultValue for course %s", courseName));
+
+            // initializing UI elements
+            view_course_sections = (ExpandableListView) findViewById(R.id.moodleCourseSections);
+            view_course_sections.setOnGroupClickListener(this);
+            view_course_sections.setOnChildClickListener(this);
+            title = (TextView) findViewById(R.id.courseTitle);
+            title.setText(courseName);
+
+            //TODO replace it with realmanager
+            mockManager = new MockMoodleManager(this);
+            sections = (List<MoodleCourseSection>) mockManager.getMoodleUserCourseInfo().getSections();
+
+            /*
+            realManager = new RealMoodleManager(this);
+            realManager.requestUserToken(this, "student", "moodle");
+            realManager.setMoodleUserToken(new MoodleToken());
+            realManager.getMoodleUserToken().setToken(userToken);
+            realManager.requestUserData(this);
+            realManager.requestUserCourseInfo(this, courseId);
+            sections = realManager.getMoodleUserCourseInfo().getSections();
+            */
+
+            dataAdapter = new MoodleCourseInfoExpandableAdapter(sections, this);
+            view_course_sections.setAdapter(dataAdapter);
+        }catch (Exception e){
+            Utils.log(e);
+            Utils.showToast(this.getApplicationContext(), "Sorry! Something went wrong!");
+            finish();
+        }
+    }
+}
---
+++ app/src/main/java/de/tum/in/tumcampus/adapters/MoodleCourseInfoExpandableAdapter.java
@@ -0,0 +1,424 @@
+package de.tum.in.tumcampus.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.tum.in.tumcampus.R;
+import de.tum.in.tumcampus.auxiliary.Utils;
+import de.tum.in.tumcampus.models.MoodleCourseContent;
+import de.tum.in.tumcampus.models.MoodleCourseModule;
+import de.tum.in.tumcampus.models.MoodleCourseSection;
+import de.tum.in.tumcampus.models.MoodleObject;
+
+/**
+ * Created by a2k on 6/11/2015.
+ * the data provided by moodle is a list of jsons for each there is a section,
+ * each section has everal modules, and each module can have several contents, which
+ * can be file, link or ...
+ * in this adapter section and modules are considered as group items, and contents as childs
+ * Therefore, sections has no child, but each module can have several childs
+ */
+public class MoodleCourseInfoExpandableAdapter extends BaseExpandableListAdapter{
+
+    private List<MoodleCourseSection> sections;
+
+   // a mapping from keys=("sections","modules") to positions in group indexes
+    private Map<String, List<Integer>> indexMapping;
+
+    // all group positions for existing sections
+    private Map<Integer, MoodleCourseSection> exisiting_sections;
+
+    // all group positions for existing modules
+    private Map<Integer, MoodleCourseModule> exisiting_modules;
+
+    private final Activity activity;
+    private LayoutInflater inflater = null;
+
+    // types for group object
+    final int SECTION_HEADER = 1000;
+    final int MODULE_HEADER = 500;
+    final int NOT_KNOWN = 0;
+
+    // types for drawable indicator
+    private static final int[] EMPTY_STATE_SET = {};
+    private static final int[] GROUP_EXPANDED_STATE_SET =
+            {android.R.attr.state_expanded};
+    private static final int[][] GROUP_STATE_SETS = {
+            EMPTY_STATE_SET, // index 0
+            GROUP_EXPANDED_STATE_SET // index 1
+    };
+
+    public static class ViewHolder {
+        public ImageView image;
+        public TextView title;
+    }
+
+    public MoodleCourseInfoExpandableAdapter(List<MoodleCourseSection> sections, Activity activity) {
+
+        if (sections == null) {
+            Utils.log("The given List of values for MoodleCourseInfoAdapter is null");
+            MoodleCourseSection temp = new MoodleCourseSection(null);
+            this.sections = new ArrayList<MoodleCourseSection>();
+            this.sections.add(temp);
+        } else
+            this.sections = sections;
+
+        make_indexMapping();
+        this.activity = activity;
+        this.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public int getGroupCount() {
+        return exisiting_sections.size() + exisiting_modules.size();
+    }
+
+    /**
+     * return 0 for Sections of the course and also for Modules which
+     have no content
+     * @param groupPosition
+     * @return
+     */
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        try {
+            if (getItemViewType(groupPosition) == MODULE_HEADER) {
+                List<MoodleCourseContent> contents = (List<MoodleCourseContent>) getModule(groupPosition).getContents();
+                if (contents != null)
+                    return contents.size();
+                else
+                    return 0;
+            } else
+                return 0;
+        }catch(Exception e){
+            Utils.log(e);
+            return 0;
+        }
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+
+        if (getItemViewType(groupPosition) == SECTION_HEADER)
+            return getSection(groupPosition);
+        else if (getItemViewType(groupPosition) == MODULE_HEADER)
+            return getModule(groupPosition);
+        else
+            Utils.log(String.format("No Group Object Found for position %s", groupPosition));
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        try {
+            if (getItemViewType(groupPosition) == MODULE_HEADER) {
+                List<MoodleCourseContent> contents = (List<MoodleCourseContent>) getModule(groupPosition).getContents();
+                if (contents != null)
+                    return contents.get(childPosition);
+                else
+                    return null;
+            }
+            return null;
+        }catch (Exception e){
+            Utils.log(e);
+            return null;
+        }
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        int type = getItemViewType(groupPosition);
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.moodle_course_headers, null);
+            viewHolder = new ViewHolder();
+            viewHolder.title = (TextView) convertView.findViewById(R.id.header);
+            viewHolder.image = (ImageView) convertView.findViewById(R.id.indicator_icon);
+            convertView.setTag(viewHolder);
+        }else
+            viewHolder = (ViewHolder)convertView.getTag();
+
+        try {
+            switch (type) {
+
+                // setting the necessary style based on the type of the group item
+                case SECTION_HEADER:
+                    MoodleCourseSection section_item = (MoodleCourseSection) getGroup(groupPosition);
+                    changeStyle(viewHolder, section_item, groupPosition, isExpanded);
+                    break;
+                case MODULE_HEADER:
+                    MoodleCourseModule module_item = (MoodleCourseModule) getGroup(groupPosition);
+                    changeStyle(viewHolder, module_item, groupPosition, isExpanded);
+                    break;
+                case NOT_KNOWN:
+                    Utils.log(String.format("Error: Item %s type not found", groupPosition));
+                    break;
+            }
+        } catch (NullPointerException e) {
+            Utils.log(e);
+            Utils.log(String.format("Error: Item %s is null", groupPosition));
+            return convertView;
+        }
+
+        return convertView;
+    }
+
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+        int type = getItemViewType(groupPosition);
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.moodle_course_content, null);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.courseChildHeader);
+            convertView.setTag(viewHolder);
+        }else
+            viewHolder = (ViewHolder)convertView.getTag();
+
+        try {
+            switch (type) {
+                case MODULE_HEADER:
+                    if (type == MODULE_HEADER) {
+                        MoodleCourseContent content = (MoodleCourseContent) getChild(groupPosition, childPosition);
+                        if (content != null) {
+                            String name = content.getFilename();
+                            viewHolder.title.setText(name);
+                        }
+                    }
+            }
+        }catch (NullPointerException e) {
+            Utils.log(e);
+            Utils.log(String.format("Error: Item %s is null", groupPosition));
+            return convertView;
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    /**
+     * gets back the type of the item in group positions of the list
+     * @param position the position of the grouo item
+     * @return (int) type of this group: module or section
+     */
+    public int getItemViewType(int position) {
+        if (sections == null)
+            return NOT_KNOWN;
+        else {
+            if (indexMapping.get("sections").contains(Integer.valueOf(position)))
+                return SECTION_HEADER;
+            else if (indexMapping.get("modules").contains(Integer.valueOf(position)))
+                return MODULE_HEADER;
+            else
+                return NOT_KNOWN;
+        }
+    }
+
+    /**
+     * This method make a mapping for indexes in the list of items shown
+     * in the activity and their type (section?module?content based on the position in th layout)
+     */
+    private void make_indexMapping() {
+        indexMapping = new HashMap<String, List<Integer>>();
+        List<Integer> sections_indexes = new ArrayList<Integer>();
+        List<Integer> modules_indexes = new ArrayList<Integer>();
+        if (sections != null) {
+            exisiting_sections = new HashMap<Integer, MoodleCourseSection>();
+            exisiting_modules = new HashMap<Integer, MoodleCourseModule>();
+
+            int index = -1;
+            for (MoodleCourseSection currentSection : sections) {
+                index++;
+                exisiting_sections.put(index, currentSection);
+                sections_indexes.add(index);
+
+                if (currentSection.getModules() != null) {
+
+                    for (MoodleCourseModule currentModule : (List<MoodleCourseModule>) currentSection.getModules()) {
+                        index++;
+                        exisiting_modules.put(index, currentModule);
+                        modules_indexes.add(index);
+                    }
+                }
+            }
+            indexMapping.put("sections", sections_indexes);
+            indexMapping.put("modules", modules_indexes);
+            Utils.log("Current mapping found for this course");
+            Utils.log("sections: " + indexMapping.get("sections").toString());
+            Utils.log("modules: " + indexMapping.get("modules").toString());
+
+        } else
+            Utils.log("cannot make index_mapping! Sections is null");
+    }
+
+
+    public MoodleCourseSection getSection(int position) {
+
+        return exisiting_sections.get(position);
+    }
+
+    public MoodleCourseModule getModule(int position) {
+        return exisiting_modules.get(position);
+    }
+
+    /**
+     * gets the drawable for related module in moodle
+     * because of efficiency used this method, retrieving resources by string name
+     * is not efficient.
+     * @param module of type MoodleCourseModule
+     * @return Drawable, the icon related to this module
+     */
+    public Drawable getImageResource(MoodleCourseModule module){
+        String modName = module.getModname();
+        switch(modName){
+            case "assign":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_assign);
+            case "assignment":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_assign);
+            case "book":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_book);
+            case "chat":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_chat);
+            case "choice":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_choice);
+            case "data":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_data);
+            case "feedback":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_feedback);
+            case "folder":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_folder);
+            case "forum":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_forum);
+            case "glossary":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_glossary);
+            case "imscp":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_imscp);
+            case "label":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_label);
+            case "lesson":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_lesson);
+            case "lti":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_lti);
+            case "page":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_page);
+            case "quiz":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_quiz);
+            case "resource":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_resource);
+            case "scorm":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_scorm);
+            case "survey":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_survey);
+            case "url":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_url);
+            case "wiki":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_wiki);
+            case "workshop":
+                return this.activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_workshop);
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * calculates the image height for the imageView of an item
+     * in the list. Based on the item type. Group, Group with children, child
+     * @param itemType
+     * @return height in pixels based on the display density of the device
+     */
+    public int getImageHeight(int itemType){
+        DisplayMetrics metrics = this.activity.getApplicationContext().getResources().getDisplayMetrics();
+        float dp, fpixels;
+        switch(itemType){
+            case SECTION_HEADER:
+                dp = 30f;
+                fpixels = metrics.density * dp;
+                return (int) (fpixels + 0.5f);
+            case MODULE_HEADER:
+                dp = 80f;
+                fpixels = metrics.density * dp;
+                return (int) (fpixels + 0.5f);
+            default:
+                return 0;
+        }
+    }
+
+    private void changeStyle(ViewHolder holder, MoodleObject object, int groupPosition, boolean isExpanded){
+
+        if (object instanceof MoodleCourseSection){
+            MoodleCourseSection section = (MoodleCourseSection) object;
+            String mod_name = section.getName();
+            holder.image.setVisibility(View.GONE);
+            holder.title.setHeight(getImageHeight(SECTION_HEADER));
+            holder.title.setText(mod_name);
+            holder.title.setBackgroundColor(Color.argb(246,244,120,3));
+        }else{
+
+            // the object is a course Module
+            MoodleCourseModule module = (MoodleCourseModule) object;
+            String mod_name = module.getName();
+            holder.title.setHeight(getImageHeight(MODULE_HEADER));
+            holder.title.setText(mod_name);
+            holder.title.setBackgroundColor(Color.WHITE);
+
+            /* if this course module has no child set the icon
+            from getImageResource, otherwise set the group indicator icon
+            */
+            if( getChildrenCount( groupPosition ) == 0 ) {
+                Drawable drawable = getImageResource(module);
+                if (drawable == null)
+                    holder.image.setVisibility(View.GONE);
+                else {
+                    holder.image.setImageDrawable(drawable);
+                    holder.image.setVisibility( View.VISIBLE );
+                }
+            }else {
+                holder.image.setVisibility( View.VISIBLE );
+                Drawable drawable = activity.getBaseContext().getResources().getDrawable(R.drawable.moodle_expander_group);
+                holder.image.setImageDrawable(drawable);
+
+                // setting the state of the group indicator
+                int stateSetIndex = ( isExpanded ? 1 : 0) ;
+                drawable.setState(GROUP_STATE_SETS[stateSetIndex]);
+            }
+        }
+    }
+}
