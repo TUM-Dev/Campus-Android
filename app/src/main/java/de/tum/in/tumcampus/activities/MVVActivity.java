66186432f1b18ac3ded9464ba12a7daeee3bc90b
arash-a2k <arash.khataei@gmail.com>
2015-06-21 14:38:34 +0200

    mvv activity UI implemented

--- app/src/main/java/de/tum/in/tumcampus/activities/MVVActivity.java
+++ app/src/main/java/de/tum/in/tumcampus/activities/MVVActivity.java
@@ -1,12 +1,16 @@
 package de.tum.in.tumcampus.activities;
 
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -16,6 +20,8 @@ import org.jsoup.select.Elements;
 
 import de.tum.in.tumcampus.R;
 import de.tum.in.tumcampus.activities.generic.ActivityForDownloadingExternal;
+import de.tum.in.tumcampus.adapters.MvvAdapter;
+import de.tum.in.tumcampus.auxiliary.Utils;
 import de.tum.in.tumcampus.models.MVVDeparture;
 import de.tum.in.tumcampus.models.MVVObject;
 import de.tum.in.tumcampus.models.MVVSuggestion;
@@ -26,44 +32,44 @@ import de.tum.in.tumcampus.models.managers.MVVJsoupParser;
  * Created by enricogiga on 15/06/2015.
  * Activity for searching timetables from the queried station using mvg live
  */
-public class MVVActivity extends ActivityForDownloadingExternal implements MVVDelegate {
+public class MVVActivity extends ActivityForDownloadingExternal implements MVVDelegate, AdapterView.OnItemClickListener {
 
-    String url = "";
-    String query = "";
-    EditText mvv_query;
-    Button btnSearch;
-
-
+    private EditText mvv_query;
+    private Button btnSearch;
+    private ListView departurelist;
+    private MvvAdapter dataAdapter;
+    private TextView listHeader;
+    private final String baseURL1 = "http://www.mvg-live.de/ims/dfiStaticAuswahl.svc?haltestelle=";
+    private final String baseURL2 = "&ubahn=checked&bus=checked&tram=checked&sbahn=checked";
 
     public MVVActivity() {
         super("MVV", R.layout.activity_mvv_main);
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        baseSetup();
 
-
-        mvv_query = (EditText) findViewById(R.id.mvv_query);
-        btnSearch = (Button) findViewById(R.id.mvv_search);
         final MVVDelegate delegate = this;
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                query = mvv_query.getText().toString();
-                url= "http://www.mvg-live.de/ims/dfiStaticAuswahl.svc?haltestelle="+query+"&ubahn=checked&bus=checked&tram=checked&sbahn=checked";
+                String query = mvv_query.getText().toString();
+                String url= buildQueryUrl(query);
                 ( new MVVJsoupParser(delegate) ).execute(new String[]{url});
             }
         });
     }
 
+    private String buildQueryUrl(String query){
+        return baseURL1 + query + baseURL2;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-
         return true;
     }
 
@@ -73,8 +79,18 @@ public class MVVActivity extends ActivityForDownloadingExternal implements MVVDe
         Log.d("SuggestionList","gut");
         for(int j = 0; j<sug.getResultList().size();j++){
             MVVSuggestion suggestion =(MVVSuggestion) sug.getResultList().get(j);
-            Log.d("Suggestion Name"+j, ""+suggestion.name);
-            Log.d("Suggestion Link"+j, suggestion.link);
+            Log.d("Suggestion Name"+j, ""+suggestion.getName());
+            Log.d("Suggestion Link"+j, suggestion.getLink());
+        }
+
+        try {
+            Utils.log(getString(R.string.mvv_suggestion_hint));
+            listHeader.setText(R.string.mvv_suggestion_hint);
+            dataAdapter = new MvvAdapter(sug, this, this);
+            departurelist.setAdapter(dataAdapter);
+        }catch (Exception e){
+            Utils.log(e);
+            Utils.showToast(this,"Sorry something went wrong");
         }
     }
 
@@ -82,11 +98,21 @@ public class MVVActivity extends ActivityForDownloadingExternal implements MVVDe
     public void showDepartureList(MVVObject dep) {
         for(int j = 0; j<dep.getResultList().size();j++){
             MVVDeparture departure =(MVVDeparture) dep.getResultList().get(j);
-            Log.d("Departure Time"+j, ""+departure.min);
-            Log.d("Departure Direction"+j, departure.direction);
-            Log.d("Departure line"+j, departure.line);
+            Log.d("Departure Time"+j, ""+departure.getMin());
+            Log.d("Departure Direction"+j, departure.getDirection());
+            Log.d("Departure line"+j, departure.getLine());
         }
+        try {
+            String stationHeader = dep.getDepartureHeader().trim() + " " + dep.getDepartureServerTime() +" Uhr";
+            listHeader.setText(stationHeader);
+            listHeader.setTypeface(null, Typeface.BOLD);
 
+            dataAdapter = new MvvAdapter(dep, this, this);
+            departurelist.setAdapter(dataAdapter);
+        }catch (Exception e){
+            Utils.log(e);
+            Utils.showToast(this,"Sorry something went wrong");
+        }
     }
 
     @Override
@@ -94,6 +120,35 @@ public class MVVActivity extends ActivityForDownloadingExternal implements MVVDe
 
         Toast.makeText(this,object.getMessage(),Toast.LENGTH_LONG).show();
 
+    }
+
+
+    public void baseSetup(){
+        try{
+            mvv_query = (EditText) findViewById(R.id.mvv_query);
+            btnSearch = (Button) findViewById(R.id.mvv_search);
+            listHeader = (TextView)findViewById(R.id.mvv_list_header);
+            departurelist = (ListView)findViewById(R.id.mvv_details);
+            departurelist.setOnItemClickListener(this);
+        }catch(Exception e){
+            Utils.log("something went wrong!");
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Utils.log("MVV ITEM CLICKED");
+        MVVObject object = (MVVObject)parent.getItemAtPosition(position);
+        if (object.isSuggestion()){
+            MVVSuggestion suggestion = (MVVSuggestion) object;
+            String suggestion_url = suggestion.getLink();
+            Utils.log("suggestion link is " + suggestion_url);
+            requestData(suggestion_url);
+        }
+    }
+
+    private void requestData(String url){
+        ( new MVVJsoupParser(this) ).execute(new String[]{url});
     }
 }
 
--- app/src/main/java/de/tum/in/tumcampus/adapters/MvvAdapter.java
+++ app/src/main/java/de/tum/in/tumcampus/adapters/MvvAdapter.java
@@ -1,27 +1,38 @@
 package de.tum.in.tumcampus.adapters;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import de.tum.in.tumcampus.R;
 import de.tum.in.tumcampus.auxiliary.Utils;
+import de.tum.in.tumcampus.models.MVVDeparture;
 import de.tum.in.tumcampus.models.MVVObject;
+import de.tum.in.tumcampus.models.MVVSuggestion;
+import de.tum.in.tumcampus.models.MoodleCourseModule;
 
 /**
  * Created by enricogiga on 17/06/2015.
  */
-public class MvvAdapter extends BaseAdapter{
+public class MvvAdapter extends BaseAdapter {
 
 
     private MVVObject data;
-    private View.OnClickListener listener;
-    public static class ViewHolder{
+    private AdapterView.OnItemClickListener listener;
+    private LayoutInflater inflater;
+    private Context currentContext;
+
+    public static class ViewHolder {
         public ImageView icon;
         public TextView number;
         public TextView station;
@@ -29,9 +40,11 @@ public class MvvAdapter extends BaseAdapter{
 
     }
 
-    public MvvAdapter(MVVObject data, View.OnClickListener listener){
+    public MvvAdapter(MVVObject data, AdapterView.OnItemClickListener listener, Context context) {
         this.data = data;
         this.listener = listener;
+        this.currentContext = context;
+        this.inflater = (LayoutInflater) currentContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -58,20 +71,76 @@ public class MvvAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
-        if (convertView == null){
-            viewHolder = new ViewHolder();
-            if (data.isSuggestion()){
+        try {
 
-            }else {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.mvv_item, null);
 
-            }
-            convertView.setTag(viewHolder);
-        }else
-            viewHolder = (ViewHolder)convertView.getTag();
+                viewHolder = new ViewHolder();
+                viewHolder.icon = (ImageView) convertView.findViewById(R.id.mvv_icon);
+                viewHolder.number = (TextView) convertView.findViewById(R.id.line_number);
+                viewHolder.station = (TextView) convertView.findViewById(R.id.station);
+                viewHolder.minutes = (TextView) convertView.findViewById(R.id.minutes);
 
+                convertView.setTag(viewHolder);
+            } else
+                viewHolder = (ViewHolder) convertView.getTag();
 
+            changeStyle(viewHolder, position);
 
+            return convertView;
+        } catch (NullPointerException e) {
+            Utils.log(e);
+            Utils.log(String.format("Error: Item %s is null", position));
+            return convertView;
+        }
+    }
+
+    private void changeStyle(ViewHolder viewHolder, int position) {
+        if (data.isSuggestion()) {
+            MVVSuggestion suggestion = (MVVSuggestion) getItem(position);
+            String name = suggestion.getName();
+
+            viewHolder.icon.setVisibility(View.GONE);
+            viewHolder.number.setVisibility(View.GONE);
+            viewHolder.minutes.setVisibility(View.GONE);
+            viewHolder.station.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+            viewHolder.station.setText(name);
+        } else if (data.isDeparture()) {
+            MVVDeparture departure = (MVVDeparture) getItem(position);
+            String number = departure.getLine();
+            String station = departure.getDirection();
+            String minutes = String.valueOf(departure.getMin() +" min");
+
+            viewHolder.icon.setVisibility(View.VISIBLE);
+            viewHolder.icon.setImageDrawable(getImageResource(departure));
+            viewHolder.number.setText(number);
+            viewHolder.minutes.setText(minutes);
+            viewHolder.station.setText(station);
+
+        }
+    }
+
+    /**
+     * gets the drawable for related departure in mvv
+     * because of efficiency used this method, retrieving resources by string name
+     * is not efficient.
+     *
+     * @param departure MVVDeparture object
+     * @return Drawable, the icon related to this departure
+     */
+    public Drawable getImageResource(MVVDeparture departure) {
+        MVVDeparture.TransportationType type = departure.getTransportationType();
+        Utils.log("type of transportation is " + type);
+        switch (type) {
+            case UBAHN:
+                return currentContext.getResources().getDrawable(R.drawable.mvv_ubahn);
+            case SBAHN:
+                return currentContext.getResources().getDrawable(R.drawable.mvv_sbahn);
+            case BUS_TRAM:
+                return currentContext.getResources().getDrawable(R.drawable.mvv_bustram);
+        }
         return null;
     }
 }
