da77f46328f607607c206734e7e68ebf185017ba
arash-a2k <arash.khataei@gmail.com>
2015-07-10 17:53:16 +0200

    mensa widget refactored

--- app/src/main/java/de/tum/in/tumcampus/services/MensaWidgetService.java
+++ app/src/main/java/de/tum/in/tumcampus/services/MensaWidgetService.java
@@ -3,9 +3,6 @@ package de.tum.in.tumcampus.services;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
-import android.text.SpannableString;
-import android.text.style.ImageSpan;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
@@ -22,7 +19,6 @@ import de.tum.in.tumcampus.models.managers.CafeteriaManager;
  * Created by a2k on 7/6/2015.
  */
 public class MensaWidgetService extends RemoteViewsService {
-    public static String ACTION_TELL_MENSA_NAME = "de.tum.in.tumcampus.services.MensaWidgetService.action.TELL_MENSA_NAME";
 
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
@@ -32,38 +28,28 @@ public class MensaWidgetService extends RemoteViewsService {
 }
 
 class MensaRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory{
+
     private Context applicationContext;
-    private int mensaAppWidgetId;
     private CafeteriaManager mensaManager;
     private List<CafeteriaMenu> mensaMenu;
     private String mensaName;
 
     public MensaRemoteViewFactory(Context applicationContext, Intent intent) {
         this.applicationContext = applicationContext;
-        mensaAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
     }
-
 
     @Override
     public void onCreate() {
         mensaManager = new CafeteriaManager(applicationContext);
+
+        // Map of the name of the best mensa and list of its Menu
         Map<String, List<CafeteriaMenu>> currentMensa = mensaManager.getBestMatchMensaInfo(applicationContext);
         if (currentMensa != null) {
             mensaName = currentMensa.keySet().iterator().next();
-            broadcastMensaName(mensaName);
             mensaMenu = currentMensa.get(mensaName);
         }else
             Utils.log("Error! Could not get list of menus for the mensa widget ");
     }
-
-    private void broadcastMensaName(String name){
-        Intent intent = new Intent();
-        intent.setAction(MensaWidgetService.ACTION_TELL_MENSA_NAME);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mensaAppWidgetId);
-        intent.putExtra("mensa_name",name);
-        LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent);
-        Utils.log("broadcasting mensa's name: " + name);
-}
 
     @Override
     public void onDataSetChanged() {
@@ -124,40 +110,6 @@ class MensaRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory{
     @Override
     public boolean hasStableIds() {
         return true;
-    }
-
-
-    /**
-     * Converts menu text to {@link android.text.SpannableString}.
-     * Replaces all (v), ... annotations with images
-     *
-     * @param context Context
-     * @param menu    Text with annotations
-     * @return Spannable text with images
-     */
-    public static SpannableString menuToSpan(Context context, String menu) {
-        int len;
-        do {
-            len = menu.length();
-            menu = menu.replaceFirst("\\(([A-Za-z0-9]+),", "($1)(");
-        } while (menu.length() > len);
-        SpannableString text = new SpannableString(menu);
-        replaceWithImg(context, menu, text, "(v)", R.drawable.meal_vegan);
-        replaceWithImg(context, menu, text, "(f)", R.drawable.meal_veggie);
-        replaceWithImg(context, menu, text, "(R)", R.drawable.meal_beef);
-        replaceWithImg(context, menu, text, "(S)", R.drawable.meal_pork);
-        replaceWithImg(context, menu, text, "(GQB)", R.drawable.ic_gqb);
-        replaceWithImg(context, menu, text, "(99)", R.drawable.meal_alcohol);
-        return text;
-    }
-
-    private static void replaceWithImg(Context context, String menu, SpannableString text, String sym, int drawable) {
-        int ind = menu.indexOf(sym);
-        while (ind >= 0) {
-            ImageSpan is = new ImageSpan(context, drawable);
-            text.setSpan(is, ind, ind + sym.length(), 0);
-            ind = menu.indexOf(sym, ind + sym.length());
-        }
     }
 
 }
--- app/src/main/java/de/tum/in/tumcampus/widgets/MensaWidget.java
+++ app/src/main/java/de/tum/in/tumcampus/widgets/MensaWidget.java
@@ -1,4 +1,4 @@
-package de.tum.in.tumcampus;
+package de.tum.in.tumcampus.widgets;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -7,34 +7,41 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
 
-import java.util.List;
-import java.util.Map;
-
-import de.tum.in.tumcampus.auxiliary.Utils;
-import de.tum.in.tumcampus.models.CafeteriaMenu;
+import de.tum.in.tumcampus.R;
 import de.tum.in.tumcampus.models.managers.CafeteriaManager;
 import de.tum.in.tumcampus.services.MensaWidgetService;
 
 
 /**
- * Implementation of App Widget functionality.
+ * Implementation of Mensa Widget functionality.
+ * The Update intervals is set to 10 hours in mensa_widget_info.xml
+ *
  */
 public class MensaWidget extends AppWidgetProvider {
 
     private RemoteViews rv;
+    AppWidgetManager appWidgetManager;
+
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
         // There may be multiple widgets active, so update all of them
         final int N = appWidgetIds.length;
+        this.appWidgetManager = appWidgetManager;
+
         for (int i = 0; i < N; i++) {
             Intent intent = new Intent(context, MensaWidgetService.class);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
             intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
             rv = new RemoteViews(context.getPackageName(), R.layout.mensa_widget);
+
+            // set the header for the Widget layout
             CafeteriaManager mensaManager = new CafeteriaManager(context);
             String mensaName = mensaManager.getBestMatchMensaName(context);
             rv.setTextViewText(R.id.mensa_widget_header, mensaName);
+
+            // set the adapter for the list view in the mensaWidget
             rv.setRemoteAdapter(appWidgetIds[i], R.id.food_item, intent);
             rv.setEmptyView(R.id.empty_view, R.id.empty_view);
             appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
@@ -43,30 +50,6 @@ public class MensaWidget extends AppWidgetProvider {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-
-    @Override
-    public void onReceive(final Context context, final Intent intent){
-        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-        Utils.log("Receieved broadcast! " + intent.getAction());
-        if (intent.getAction().equals(MensaWidgetService.ACTION_TELL_MENSA_NAME)){
-            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-            String mensaName = intent.getStringExtra("mensa_name");
-            Utils.log("got mensa name from broadcast !" + mensaName);
-            rv.setTextViewText(R.id.mensa_widget_header, mensaName);
-        }
-        super.onReceive(context, intent);
-    }
 }
 
 
