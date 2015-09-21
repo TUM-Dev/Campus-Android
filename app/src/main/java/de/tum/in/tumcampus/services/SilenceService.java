728e301ae8399bc9c97c91256d6668a58987a5e4
fent <philipp@fent.de>
2015-09-22 01:34:10 +0200

    refactor the SilentService remove code duplication getWaitDuration and use a ScheduledExecutor instead of manual Thread waiting

--- app/src/main/java/de/tum/in/tumcampus/services/SilenceService.java
+++ app/src/main/java/de/tum/in/tumcampus/services/SilenceService.java
@@ -6,106 +6,114 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.media.AudioManager;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import de.tum.in.tumcampus.auxiliary.Const;
 import de.tum.in.tumcampus.auxiliary.Utils;
 import de.tum.in.tumcampus.models.managers.CalendarManager;
 
-/** Service used to silence the mobile during lectures */
+/**
+ * Service used to silence the mobile during lectures
+ */
 public class SilenceService extends IntentService {
 
-	/**
-	 * Interval in milliseconds to check for current lectures
-	 */
-	private static final int CHECK_INTERVAL = 60000 * 15, // 15 Minutes
-				CHECK_DELAY = 10000; // 10 Seconds after Calendar changed
-	private static final String SILENCE_SERVICE = "SilenceService";
+    /**
+     * Interval in milliseconds to check for current lectures
+     */
+    private static final int CHECK_INTERVAL = 60000 * 15, // 15 Minutes
+            CHECK_DELAY = 10000; // 10 Seconds after Calendar changed
+    private static final String SILENCE_SERVICE = "SilenceService";
 
-	/**
-	 * default init (run intent in new thread)
-	 */
-	public SilenceService() {
-		super(SILENCE_SERVICE);
-	}
+    /**
+     * default init (run intent in new thread)
+     */
+    public SilenceService() {
+        super(SILENCE_SERVICE);
+    }
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		Utils.log("SilenceService has started");
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.log("SilenceService has started");
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		Utils.log("SilenceService has stopped");
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Utils.log("SilenceService has stopped");
+    }
 
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		// loop until silence mode gets disabled in settings
-		while (true) {
-			try {
-				int wait_duration = CHECK_INTERVAL;
-				if (Utils.getSettingBool(this, Const.SILENCE_SERVICE, false)) {
-					Utils.log("SilenceService enabled, checking for lectures ...");
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        final ScheduledExecutorService tpe = Executors.newSingleThreadScheduledExecutor();
+        tpe.schedule(new Runnable() {
+            @Override
+            public void run() {
+                long wait_duration = CHECK_INTERVAL;
 
-					AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                //Abort, if the settings changed
+                if (!Utils.getSettingBool(SilenceService.this, Const.SILENCE_SERVICE, false)) {
+                    return;
+                }
+                Utils.log("SilenceService enabled, checking for lectures ...");
 
-					CalendarManager calendarManager = new CalendarManager(this);
-					if (!calendarManager.hasLectures()) {
-						Utils.logv("No lectures available");
-						return;
-					}
+                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-					Cursor cursor = calendarManager.getCurrentFromDb();
-					Utils.log("Current lectures: " + String.valueOf(cursor.getCount()));
-					
-					if (cursor.getCount() != 0) {
-						// if current lecture(s) found, silence the mobile
-						Utils.setInternalSetting(this, Const.SILENCE_ON, true);
+                CalendarManager calendarManager = new CalendarManager(SilenceService.this);
+                if (!calendarManager.hasLectures()) {
+                    Utils.logv("No lectures available");
+                    return;
+                }
 
-                        // Set into silent mode
-                        String mode = Utils.getSetting(this, "silent_mode_set_to", "0");
-                        if(mode.equals("0")) {
-                            Utils.log("set ringer mode: vibration");
-                            am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-                        } else {
-                            Utils.log("set ringer mode: silent");
-                            am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-                        }
-                        			// refresh when event has ended
-                        			wait_duration = (int) Math.min(wait_duration,
-                        				(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH).parse(cursor.getString(3))).getTime()
-                        				- System.currentTimeMillis() 
-                        				+ CHECK_DELAY
-                        				);
-					} else if (Utils.getInternalSettingBool(this, Const.SILENCE_ON, false)) {
-						// default: no silence
-						Utils.log("set ringer mode: normal");
-						am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-						Utils.setInternalSetting(this, Const.SILENCE_ON, false);
-						
-						
-						Cursor cursor2 = calendarManager.getNextCalendarItem();
-						// refresh when next event has started
-						wait_duration = (int) Math.min(wait_duration,
-                        				(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH).parse(cursor2.getString(1))).getTime()
-                        				- System.currentTimeMillis() 
-                        				+ CHECK_DELAY
-                        				);
-                        			cursor2.close();
-					}
-					cursor.close();
-				}
-				// wait until next check
-				synchronized (this) {
-					wait(wait_duration);
-				}
-			} catch (Exception e) {
-				Utils.log(e, "");
-			}
-		}
-	}
+                Cursor cursor = calendarManager.getCurrentFromDb();
+                Utils.log("Current lectures: " + String.valueOf(cursor.getCount()));
+
+                if (cursor.getCount() != 0) {
+                    // if current lecture(s) found, silence the mobile
+                    Utils.setInternalSetting(SilenceService.this, Const.SILENCE_ON, true);
+
+                    // Set into silent mode
+                    String mode = Utils.getSetting(SilenceService.this, "silent_mode_set_to", "0");
+                    if (mode.equals("0")) {
+                        Utils.log("set ringer mode: vibration");
+                        am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                    } else {
+                        Utils.log("set ringer mode: silent");
+                        am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                    }
+                    // refresh when event has ended
+                    wait_duration = getWaitDuration(cursor.getString(3));
+                } else if (Utils.getInternalSettingBool(SilenceService.this, Const.SILENCE_ON, false)) {
+                    // default: no silence
+                    Utils.log("set ringer mode: normal");
+                    am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                    Utils.setInternalSetting(SilenceService.this, Const.SILENCE_ON, false);
+
+
+                    Cursor cursor2 = calendarManager.getNextCalendarItem();
+                    // refresh when next event has started
+                    wait_duration = getWaitDuration(cursor2.getString(1));
+                    cursor2.close();
+                }
+                cursor.close();
+
+                tpe.schedule(this, wait_duration, TimeUnit.MILLISECONDS);
+            }
+        }, 0, TimeUnit.MILLISECONDS);
+    }
+
+    private static long getWaitDuration(String timeToEventString) {
+        long timeToEvent = Long.MAX_VALUE;
+        try {
+            timeToEvent = (new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH).parse(timeToEventString)).getTime();
+        } catch (ParseException e) {
+            Utils.log(e, "");
+        }
+        return Math.min(CHECK_INTERVAL, timeToEvent - System.currentTimeMillis() + CHECK_DELAY);
+    }
 }
