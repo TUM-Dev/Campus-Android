f64cd363c2b8145a5cb9c9bd2a64e64dd9480a14
pfent <philipp@fent.de>
2016-05-01 13:13:47 +0200

    fix the search suggestion providers not working

--- app/src/main/java/de/tum/in/tumcampusapp/auxiliary/LectureSearchSuggestionProvider.java
+++ app/src/main/java/de/tum/in/tumcampusapp/auxiliary/LectureSearchSuggestionProvider.java
@@ -4,7 +4,7 @@ package de.tum.in.tumcampusapp.auxiliary;
  * Suggestion provider for {@link de.tum.in.tumcampusapp.activities.LecturesPersonalActivity}
  */
 public class LectureSearchSuggestionProvider extends EnhancedSearchRecentSuggestionsProvider {
-	public final static String AUTHORITY = "de.tum.in.tumcampus.auxiliary.LectureSearchSuggestionProvider";
+	public final static String AUTHORITY = "de.tum.in.tumcampusapp.auxiliary.LectureSearchSuggestionProvider";
 
 	public LectureSearchSuggestionProvider() {
 		setupSuggestions("lecture", AUTHORITY);
