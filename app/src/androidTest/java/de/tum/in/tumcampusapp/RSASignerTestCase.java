47c418592dc5f04affe4a928854d558b1d3f2c67
pfent <philipp@fent.de>
2016-05-31 13:14:39 +0200

    update unit tests to JUnit 4

--- app/src/androidTest/java/de/tum/in/tumcampusapp/RSASignerTestCase.java
+++ app/src/androidTest/java/de/tum/in/tumcampusapp/RSASignerTestCase.java
@@ -1,8 +1,12 @@
 package de.tum.in.tumcampusapp;
 
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Base64;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -15,105 +19,115 @@ import de.tum.in.tumcampusapp.auxiliary.RSASigner;
 import de.tum.in.tumcampusapp.auxiliary.Utils;
 import de.tum.in.tumcampusapp.models.ChatMessage;
 
-public class RSASignerTestCase extends TestCase {
-	
-	private RSASigner signer;
-	private PrivateKey privateKeyFixture;
-	private ArrayList<ChatMessage> messageFixtures;
-	
-	
-	private PrivateKey buildPrivateKey(String privateKeyString) {
-		byte[] privateKeyBytes = Base64.decode(privateKeyString, Base64.DEFAULT);
-		KeyFactory keyFactory;
-		try {
-			keyFactory = KeyFactory.getInstance("RSA");
-			PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-			return keyFactory.generatePrivate(privateKeySpec);
-		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-			Utils.log(e);
-		}
-		return null;
-	}
-	
-	
-	protected void setUp() throws Exception {
-		privateKeyFixture = buildPrivateKey(
-			"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAM46wq9uOF7y1dmNO3nY8D1P6bCe" +
-			"t3izsm2GKQtvWmV78WbBxk1rZI2GNExvZ3aVg4mb6jOToGzm+jdNiWR07kBFSlrgNC5zq7Jmm0gz" +
-			"yTgrou6eV5NoQsPO2fI3tworvxgGa886Hu0U8p3gFk+BJnffndSq8DdBKcIjGDunTv4dAgMBAAEC" +
-			"gYB3QywLX+ZhonVhVneqw3ZLPseaSG858lGhXRCneEICpma4Uh9n7k88OPxNp69huJ1VG0GZiiog" +
-			"UIMrMD/gRG7y2NJxKkDbR2aVB+YR6aGAWYfwZVDM+Swxe3wUbDfRsAuXGapKCXnjHOKGbUCi/gx9" +
-			"AvFH4YJ9IImFGD+T7jSogQJBAP/oBCSXhDCHQXoN3vqdncmNNKl1J9QvzPY2ESMzfbdJ4QESb4YZ" +
-			"f88ixXUZpb3JVamW4WI6QHEHGMnGZwJyL6UCQQDOThaq2Mw/8QyPiKup7B+QT7/Bz7wpQx7IHw6a" +
-			"r5pX5AYAO7WmihxOgDMX6VfhCJlksxfLTVSuiJdXuJSm90sZAkBdXuJkF4R70F3rkrQQ7QFtUMAu" +
-			"NDjcCrTWANQv69Gq1qHqKjfWzeb8RMuW9kyq+pLu1cZWeLqaguRgequLEO6hAkBsq1NjUOldsQI1" +
-			"xP7vdbI2mNtgIqVxcqqPLVTLBD6flzvV+Z24iL1aWEsRiFdC8P2jvnaFH0nA2bAmg9LBlDdZAkEA" +
-			"9tg4rD0aCHQZ6kEquwN6emc9QM0X6DR0dx6Bqq8CGDkVdk0hXHBR9VUBGE4YSsxpn+LnyWSWyJum" +
-			"dWuepeUKig==");
-		
-		messageFixtures = new ArrayList<>();
-		messageFixtures.add(buildChatMessage(
-			"This is a message!",
-			"MwBZFVhzIGehiGAVaoxp0k04BJN8YyyqlPQg1hXwg1bQxgjtEXz6KsVzYOWo40/TdhcbUHo+hUhk\n" +
-			"/rLLBrkFldQuNGhd/ltwiMeN2KwdLYm5nl9DWIjPXXviCBogkVtwrBdAhgknr5Kn5Zy4TbGdMr9z\n" +
-			"d/iOl27L7GYepazgNW8="));
-		messageFixtures.add(buildChatMessage(
-			"A message with German characters: öäüßÖÄÜ!",
-			"qCOAmFho4tKW24qn6vv6j0x4jc3OVKPbVVm7EqYeJKBStGOmEcx6Crtx0MEMFxNe4Zyqo0kYMXNO\n" +
-			"/NPTvhUJlAr5x6Hlc1iKWBT5eGE8F3mKE8pTSObrCWhEBXylQjkwej5eQpahW+uexZWzeme702V0\n" +
-			"1C3FoeYUC9rSOfLlyss="));
-		messageFixtures.add(buildChatMessage(
-			"This is a Korean message: \uC88B\uC740 \uAC8C\uC784",
-			"L6DPFzKiVWrO3TeAjJwPNtC4U5D69ODloH3zmCXCpZx+fiZhopzC5cUAeolm2l/++KYZu3vR6IJK\n" +
-			"HLjXaMd4jDruY8DiWNmCbOnR/ywHQ96sCuMcdfhot5AgM05NbBH7GiAFBDJQzDejuK7M7hGmHZ6s\n" +
-			"L4WYKETiNiP1Oc6d58w="));
-	}
-	
-	protected ChatMessage buildChatMessage(String text, String signature) {
-		ChatMessage message = new ChatMessage(text);
-		message.setSignature(signature);
-		
-		return message;
-	}
-	
-	/**
-	 * Tests that a valid ASCII-based message is correctly signed.
-	 */
-	public void testAsciiMessageSigning() {
-		signer = new RSASigner(privateKeyFixture);
-		ChatMessage message = messageFixtures.get(0);
-		
-		assertEquals(message.getSignature(), signer.sign(message.getText()));
-	}
-	
-	/**
-	 * Tests that a unicode (european) message is correctly signed.
-	 */
-	public void testUnicodeMessageSigning() {
-		signer = new RSASigner(privateKeyFixture);
-		ChatMessage message = messageFixtures.get(1);
-		
-		assertEquals(message.getSignature(), signer.sign(message.getText()));
-	}
-	
-	/**
-	 * Tests that a unicode (korean) message is correctly signed.
-	 */
-	public void testUnicodeKoreanMessageSigning() {
-		signer = new RSASigner(privateKeyFixture);
-		ChatMessage message = messageFixtures.get(2);
-		
-		assertEquals(message.getSignature(), signer.sign(message.getText()));
-	}
-	
-	/**
-	 * Tests that when the private key associated with 
-	 * the signer is null, the signer returns null.
-	 */
-	public void testPrivateKeyNull() {
-		signer = new RSASigner(null);
-		ChatMessage message = messageFixtures.get(0);
-		
-		assertNull(signer.sign(message.getText()));
-	}
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class RSASignerTestCase {
+
+    private RSASigner signer;
+    private PrivateKey privateKeyFixture;
+    private ArrayList<ChatMessage> messageFixtures;
+
+
+    private PrivateKey buildPrivateKey(String privateKeyString) {
+        byte[] privateKeyBytes = Base64.decode(privateKeyString, Base64.DEFAULT);
+        KeyFactory keyFactory;
+        try {
+            keyFactory = KeyFactory.getInstance("RSA");
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            return keyFactory.generatePrivate(privateKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            Utils.log(e);
+        }
+        return null;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        privateKeyFixture = buildPrivateKey(
+                "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAM46wq9uOF7y1dmNO3nY8D1P6bCe" +
+                        "t3izsm2GKQtvWmV78WbBxk1rZI2GNExvZ3aVg4mb6jOToGzm+jdNiWR07kBFSlrgNC5zq7Jmm0gz" +
+                        "yTgrou6eV5NoQsPO2fI3tworvxgGa886Hu0U8p3gFk+BJnffndSq8DdBKcIjGDunTv4dAgMBAAEC" +
+                        "gYB3QywLX+ZhonVhVneqw3ZLPseaSG858lGhXRCneEICpma4Uh9n7k88OPxNp69huJ1VG0GZiiog" +
+                        "UIMrMD/gRG7y2NJxKkDbR2aVB+YR6aGAWYfwZVDM+Swxe3wUbDfRsAuXGapKCXnjHOKGbUCi/gx9" +
+                        "AvFH4YJ9IImFGD+T7jSogQJBAP/oBCSXhDCHQXoN3vqdncmNNKl1J9QvzPY2ESMzfbdJ4QESb4YZ" +
+                        "f88ixXUZpb3JVamW4WI6QHEHGMnGZwJyL6UCQQDOThaq2Mw/8QyPiKup7B+QT7/Bz7wpQx7IHw6a" +
+                        "r5pX5AYAO7WmihxOgDMX6VfhCJlksxfLTVSuiJdXuJSm90sZAkBdXuJkF4R70F3rkrQQ7QFtUMAu" +
+                        "NDjcCrTWANQv69Gq1qHqKjfWzeb8RMuW9kyq+pLu1cZWeLqaguRgequLEO6hAkBsq1NjUOldsQI1" +
+                        "xP7vdbI2mNtgIqVxcqqPLVTLBD6flzvV+Z24iL1aWEsRiFdC8P2jvnaFH0nA2bAmg9LBlDdZAkEA" +
+                        "9tg4rD0aCHQZ6kEquwN6emc9QM0X6DR0dx6Bqq8CGDkVdk0hXHBR9VUBGE4YSsxpn+LnyWSWyJum" +
+                        "dWuepeUKig==");
+
+        messageFixtures = new ArrayList<>();
+        messageFixtures.add(buildChatMessage(
+                "This is a message!",
+                "Tw7Geajto7C/orsLT4TfNCUa1gnu6pGumfp+Nck7/QoOmDxilgQCpuzlpxa5Y7xuQ2rQB4XhFSm4\n" +
+                        "3gOHijTwF91SQx2sdIWClofzr/H0JABpQRkkMbsVQikwOnQYp+d9c1eylNPeendoYW4NAEBKpNyw\n" +
+                        "ShtHN6jcC2Usw1lAfxE=\n"));
+        messageFixtures.add(buildChatMessage(
+                "A message with German characters: öäüßÖÄÜ!",
+                "tSHKrusEPatW7CUJGbPjLfpPkoO/hQnJPMCQDztVjQJNqpEk+Jbm+FTwakOQ49OaMtmZTfnKUoJQ\n" +
+                        "MBwgp/I6zL7Xlafxiw+jA72ah/kvixm46VlpGFF2sEYfC0Ts3Agyq1T7XXYgkrGKjC3vs6sGNFGv\n" +
+                        "IefIoEAOGaWIfZnnbuM=\n"));
+        messageFixtures.add(buildChatMessage(
+                "This is a Korean message: \uC88B\uC740 \uAC8C\uC784",
+                "Td+E1WOg5FweCrBKzsjjVbbf3EeiNLu/PWID1Tg41ak5NFllqsFUcPEzPP0bZ+Dpv0sU7deQ9BaQ\n" +
+                        "lNVaNClQsI7Y5jTmoqS5elRdrig+eq9Qzl7bvEr0EI5CUvwLZJU4LCpLYUJEGD++IOzE0xZxB6/j\n" +
+                        "MES0525W5YVR0knzoKw=\n"));
+    }
+
+    protected ChatMessage buildChatMessage(String text, String signature) {
+        ChatMessage message = new ChatMessage(text);
+        message.setSignature(signature);
+
+        return message;
+    }
+
+    /**
+     * Tests that a valid ASCII-based message is correctly signed.
+     */
+    @Test
+    public void testAsciiMessageSigning() {
+        signer = new RSASigner(privateKeyFixture);
+        ChatMessage message = messageFixtures.get(0);
+
+        Utils.log("" + message.getSignature().equals(signer.sign(message.getText())));
+        Utils.log(signer.sign(message.getText()));
+        assertEquals(message.getSignature(), signer.sign(message.getText()));
+    }
+
+    /**
+     * Tests that a unicode (european) message is correctly signed.
+     */
+    @Test
+    public void testUnicodeMessageSigning() {
+        signer = new RSASigner(privateKeyFixture);
+        ChatMessage message = messageFixtures.get(1);
+
+        assertEquals(message.getSignature(), signer.sign(message.getText()));
+    }
+
+    /**
+     * Tests that a unicode (korean) message is correctly signed.
+     */
+    @Test
+    public void testUnicodeKoreanMessageSigning() {
+        signer = new RSASigner(privateKeyFixture);
+        ChatMessage message = messageFixtures.get(2);
+
+        assertEquals(message.getSignature(), signer.sign(message.getText()));
+    }
+
+    /**
+     * Tests that when the private key associated with
+     * the signer is null, the signer returns null.
+     */
+    public void testPrivateKeyNull() {
+        signer = new RSASigner(null);
+        ChatMessage message = messageFixtures.get(0);
+
+        assertNull(signer.sign(message.getText()));
+    }
 }
