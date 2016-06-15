adc735f9896fc750a5ec61f322bc5d154306f1fc
Kordian Bruck <k@bruck.me>
2016-06-15 18:58:37 +0200

    Fixed #378

--- app/src/main/java/de/tum/in/tumcampusapp/models/managers/LocationManager.java
+++ app/src/main/java/de/tum/in/tumcampusapp/models/managers/LocationManager.java
@@ -323,7 +323,7 @@ public class LocationManager {
         }
 
         ArrayList<HashMap<String, String>> request = requestHandler.fetchRooms(loc);
-        if (request.size() > 0) {
+        if (request != null && request.size() > 0) {
             String room = request.get(0).get(TUMRoomFinderRequest.KEY_ARCH_ID);
             return requestHandler.fetchCoordinates(room);
         }
