762c482887cbc4827f858bec5b4f2756e1ed85f4
pfent <philipp@fent.de>
2016-08-07 15:30:12 +0200

    revert removing of GradeCharts

--- app/src/main/java/de/tum/in/tumcampusapp/models/ExamList.java
+++ app/src/main/java/de/tum/in/tumcampusapp/models/ExamList.java
@@ -6,19 +6,23 @@ import org.simpleframework.xml.Root;
 import java.util.List;
 
 /**
- * Wrapper class holding a list of tuitions ; based on ExamList
- *
+ * Wrapper class holding a list of exams.
+ * <p/>
  * Note: This model is based on the TUMOnline web service response format for a
  * corresponding request.
  */
 @Root(name = "rowset")
-public class TuitionList {
+public class ExamList {
 
-	@ElementList(inline = true)
-	private List<Tuition> tuitions;
+    @ElementList(inline = true)
+    private List<Exam> grades;
 
-	public List<Tuition> getTuitions() {
-		return tuitions;
-	}
+    public List<Exam> getExams() {
+        return grades;
+    }
+
+    public void setExams(List<Exam> grades) {
+        this.grades = grades;
+    }
 
 }
