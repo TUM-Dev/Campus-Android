8bf9983fe416a7d833320d887fa23dfdd40b44d0
pfent <philipp@fent.de>
2016-09-16 11:40:31 +0200

    update the GradesActivity and the GradeChartActivity to have a NavigationDrawer

--- app/src/main/java/de/tum/in/tumcampusapp/activities/GradeChartActivity.java
+++ app/src/main/java/de/tum/in/tumcampusapp/activities/GradeChartActivity.java
@@ -2,11 +2,11 @@ package de.tum.in.tumcampusapp.activities;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.activities.generic.BaseActivity;
 import de.tum.in.tumcampusapp.auxiliary.ImplicitCounter;
 
 /**
@@ -14,14 +14,17 @@ import de.tum.in.tumcampusapp.auxiliary.ImplicitCounter;
  * <p/>
  * NEEDS: chartContent set in incoming bundle (html content to show)
  */
-public class GradeChartActivity extends AppCompatActivity {
+public class GradeChartActivity extends BaseActivity {
+
+    public GradeChartActivity() {
+        super(R.layout.activity_gradechart);
+    }
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ImplicitCounter.count(this);
-        setContentView(R.layout.activity_gradechart);
         WebView Webview = (WebView) findViewById(R.id.webView1);
 
         Bundle extras = getIntent().getExtras();
