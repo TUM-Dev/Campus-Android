c476ea5abcd462c36910319421ad54d9204f55d8
Riccardo Padovani <riccardo@rpadovani.com>
2016-11-12 19:40:28 +0100

    Do not speak if there are more than 20 words

--- app/src/main/java/de/tum/in/tumcampusapp/activities/AssistantActivity.java
+++ app/src/main/java/de/tum/in/tumcampusapp/activities/AssistantActivity.java
@@ -187,7 +187,11 @@ public class AssistantActivity extends AppCompatActivity implements View.OnClick
     @SuppressWarnings("deprecation")
     private void receiveMessage(String text) {
         assistantHistoryAdapter.addElement(new ChatMessage(text, assistant));
-        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        int countWords = text.length() - text.replace(" ", "").length();
+        if (countWords < 20) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+
         rvMessageHistory.smoothScrollToPosition(rvMessageHistory.getAdapter().getItemCount());
     }
 
