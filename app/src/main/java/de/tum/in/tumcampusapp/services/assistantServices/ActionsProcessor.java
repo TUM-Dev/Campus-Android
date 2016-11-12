e6fd494c2e5401ce95ecad6f4913caf3a4312b4f
Riccardo Padovani <riccardo@rpadovani.com>
2016-11-12 15:00:55 +0100

    Add transportation location, fix the missing 0 before minutes < 10

--- app/src/main/java/de/tum/in/tumcampusapp/services/assistantServices/ActionsProcessor.java
+++ app/src/main/java/de/tum/in/tumcampusapp/services/assistantServices/ActionsProcessor.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import de.tum.in.tumcampusapp.auxiliary.luis.Action;
 import de.tum.in.tumcampusapp.auxiliary.luis.DataType;
 import de.tum.in.tumcampusapp.managers.TransportManager;
+import de.tum.in.tumcampusapp.managers.LocationManager;
 
 public class ActionsProcessor {
 
@@ -12,6 +13,8 @@ public class ActionsProcessor {
         switch (a.getActionType()){
             case TRANSPORTATION_TIME:
                 return processTransTimeAction(context, a);
+            case TRANSPORTATION_LOCATION:
+                return processTransLocationAction(context, a);
             default:
                 return "Sorry I didn't understand you, could you please ask again?";
         }
@@ -31,7 +34,8 @@ public class ActionsProcessor {
         if(departure != null) {
             return ("The " + time + " " + type + " will depart in " +
                     printCountdown(departure.countDown) +
-                    " , at " + departure.date.getHours() + ":" + departure.date.getMinutes() + ".");
+                    " , at " + String.format("%02d", departure.date.getHours()) +
+                    ":" + String.format("%02d", departure.date.getMinutes()) + ".");
         }
         return "Error parsing MVG data.";
     }
@@ -42,7 +46,17 @@ public class ActionsProcessor {
         if(h < 1){
             return m + " minutes";
         }else{
-            return h + " hours " + m + " minutes";
+            return String.format("%02d", h) + " hours " + String.format("%02d", m) + " minutes";
         }
+    }
+
+    private static String processTransLocationAction(Context context, Action a){
+        String type = a.getData(DataType.TRANSPORTATION_TYPE);
+        LocationManager locMan = new LocationManager(context);
+        String location = locMan.getStationForAssistent();
+        if(location != null) {
+            return "The nearest " + type + " station is " + location + ".";
+        }
+        return "Error parsing MVG data.";
     }
 }
