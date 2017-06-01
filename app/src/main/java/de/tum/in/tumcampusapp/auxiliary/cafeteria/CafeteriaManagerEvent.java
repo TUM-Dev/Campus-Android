9e0f7cdb31222d3cd4f1a5f990906276d832edbb
tmk241 <thomash241@gmail.com>
2017-06-02 01:10:44 +0200

    Changed favorite food notification structure

---
+++ app/src/main/java/de/tum/in/tumcampusapp/auxiliary/cafeteria/CafeteriaManagerEvent.java
@@ -0,0 +1,13 @@
+package de.tum.in.tumcampusapp.auxiliary.cafeteria;
+
+import java.util.EventObject;
+
+/**
+ * Created by Thomas-Notebook on 29.05.2017.
+ */
+/*
+public class CafeteriaManagerEvent extends EventObject{
+    public CafeteriaManagerEvent(Object source){
+        super(source);
+    }
+}*/
---
+++ app/src/main/java/de/tum/in/tumcampusapp/auxiliary/cafeteria/CafeteriaManagerListener.java
@@ -0,0 +1,9 @@
+package de.tum.in.tumcampusapp.auxiliary.cafeteria;
+
+/**
+ * Created by Thomas-Notebook on 29.05.2017.
+ */
+/*
+public interface CafeteriaManagerListener {
+    public void eventReceived(CafeteriaManagerEvent evt);
+}*/
