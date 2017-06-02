a9f7c479177578ace358fdb06787ce1c6a790afe
Yakuky <pbj0505@gmail.com>
2017-06-02 17:28:02 +0200

    Add barrier free information page to drawer.

---
+++ app/src/main/java/de/tum/in/tumcampusapp/activities/BarrierFreeInfoActivity.java
@@ -0,0 +1,13 @@
+package de.tum.in.tumcampusapp.activities;
+
+import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.activities.generic.BaseActivity;
+
+
+public class BarrierFreeInfoActivity extends BaseActivity {
+
+    public BarrierFreeInfoActivity(){
+        super(R.layout.activity_barrier_free_info);
+    }
+
+}
