6ea14d1c883d691930b21b9d55824c4d55b443b2
Deepak Raj Edayillam <deepakraje405@gmail.com>
2017-06-21 17:24:33 +0200

    initial commit

--- app/src/main/java/de/tum/in/tumcampusapp/activities/wizard/GradingActivity.java
+++ app/src/main/java/de/tum/in/tumcampusapp/activities/wizard/GradingActivity.java
@@ -3,7 +3,7 @@ package de.tum.in.tumcampusapp.activities.wizard;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import de.tum.in.tumcampusapp.R;
+import static de.tum.in.tumcampusapp.R.layout.activity_grading;
 
 
 public class GradingActivity extends AppCompatActivity {
@@ -11,7 +11,7 @@ public class GradingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grading);
+        setContentView(activity_grading);
 
 
     }
