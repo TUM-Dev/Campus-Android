d2327ba9b77bb2c9f3c38fb068e62e412a43856e
pfent <philipp@fent.de>
2017-11-08 14:17:35 +0100

    add default values for Identity so simplexml doesn't crash our app

--- app/src/main/java/de/tum/in/tumcampusapp/models/tumo/Identity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/models/tumo/Identity.kt
@@ -4,10 +4,10 @@ import org.simpleframework.xml.Element
 import org.simpleframework.xml.Root
 
 @Root(name = "row")
-data class Identity(@field:Element var vorname: String,
-                    @field:Element var familienname: String,
-                    @field:Element var kennung: String,
-                    @field:Element var obfuscated_id: String,
-                    @field:Element var obfuscated_ids: ObfuscatedIds) {
+data class Identity(@field:Element var vorname: String = "",
+                    @field:Element var familienname: String = "",
+                    @field:Element var kennung: String = "",
+                    @field:Element var obfuscated_id: String = "",
+                    @field:Element var obfuscated_ids: ObfuscatedIds = ObfuscatedIds()) {
     override fun toString(): String = "$vorname $familienname"
 }
