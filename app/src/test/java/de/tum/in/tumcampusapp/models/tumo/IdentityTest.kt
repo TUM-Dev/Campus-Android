97eaa586a1cd3ab9d4f68f4ab6fca0901d5ef061
Anselm Binninger <anselm.binninger@BWK-TECHNIK.DE>
2017-11-11 11:56:30 +0100

    Fix ValueRequiredException in startup wizard
    Set all obfuscated id fields to not being required to prevent
    a VRE in startup wizard when fields bedienstete and extern aren't
    set in the response.
    
    Fixes #576

---
+++ app/src/test/java/de/tum/in/tumcampusapp/models/tumo/IdentityTest.kt
@@ -0,0 +1,43 @@
+package de.tum.`in`.tumcampusapp.models.tumo
+
+import de.tum.`in`.tumcampusapp.tumonline.TUMOnlineConst
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.simpleframework.xml.core.Persister
+
+@RunWith(RobolectricTestRunner::class)
+class IdentityTest {
+
+    @Test
+    fun testParsingXML() {
+        val res = Persister().read(TUMOnlineConst.IDENTITY.response, XML_RESPONSE)
+        assert(res.ids.size == 1)
+        val identity = res.ids[0]
+        assert(identity.familienname == FAMILIENNAME_EXPECTED)
+        assert(identity.vorname == VORNAME_EXPECTED)
+        assert(identity.kennung == KENNUNG_EXPECTED)
+        assert(identity.obfuscated_id == OBFUSCATED_ID_EXPECTED)
+    }
+
+    companion object {
+        private const val KENNUNG_EXPECTED = "anId"
+        private const val VORNAME_EXPECTED = "Max"
+        private const val FAMILIENNAME_EXPECTED = "Mustermann"
+        private const val OBFUSCATED_ID_EXPECTED = "anobfuscatedID"
+        private const val XML_RESPONSE = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "                                                                   <rowset>\n" +
+                "                                                                   <row>\n" +
+                "                                                                   <kennung>$KENNUNG_EXPECTED</kennung>\n" +
+                "                                                                   <vorname>$VORNAME_EXPECTED</vorname>\n" +
+                "                                                                   <familienname>$FAMILIENNAME_EXPECTED</familienname>\n" +
+                "                                                                   <obfuscated_id>$OBFUSCATED_ID_EXPECTED</obfuscated_id>\n" +
+                "                                                                   <obfuscated_ids>\n" +
+                "                                                                   <studierende>asad</studierende>\n" +
+                "                                                                   <bedienstete isnull=\"true\"></bedienstete>\n" +
+                "                                                                   <extern isnull=\"true\"></extern>\n" +
+                "                                                                   </obfuscated_ids>\n" +
+                "                                                                   </row>\n" +
+                "                                                                   </rowset>"
+    }
+}
