8757fe5681108a3f05cc7c88837098362a2192e2
Paulius Šukys <paul.sukys@gmail.com>
2017-12-11 17:58:30 +0100

    Change from master to init pull request

---
+++ app/src/main/java/de/tum/in/tumcampusapp/database/dataAccessObjects/NewsDao.java
@@ -0,0 +1,8 @@
+package de.tum.in.tumcampusapp.database.dataAccessObjects;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+public class NewsDao {
+}
