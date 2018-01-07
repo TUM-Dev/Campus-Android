02ad12dc3efadd9d16123fb5145116a221f3fe73
Jacqueline Bronger <jacqueline8711@gmx.de>
2018-01-08 00:21:03 +0100

    improved Eduroam config checks (#604)
    * improved eduroam config checks + small fixes
    * reformatting + no certificate check possible

--- app/src/main/java/de/tum/in/tumcampusapp/auxiliary/Const.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/auxiliary/Const.kt
@@ -132,5 +132,5 @@ object Const {
     const val FILL_CACHE_SERVICE_JOB_ID = 1005
 
     const val EXTRA_FOREIGN_CONFIGURATION_EXISTS = "CONFIGURED_BY_OTHER_APP"
-
+    const val TUM_ID_PATTERN = "^[a-z]{2}[0-9]{2}[a-z]{3}$"
 }
