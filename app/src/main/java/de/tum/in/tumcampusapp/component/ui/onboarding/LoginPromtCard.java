ec610b65e88b70af4a433139d075cbb9a99d4517
Kordian Bruck <k@bruck.me>
2018-04-11 18:39:41 +0200

    Apply card changes to LoginPromptCard

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/onboarding/LoginPromtCard.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/onboarding/LoginPromtCard.java
@@ -3,6 +3,7 @@ package de.tum.in.tumcampusapp.component.ui.onboarding;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import de.tum.in.tumcampusapp.R;
 import de.tum.in.tumcampusapp.component.ui.overview.CardManager;
 import de.tum.in.tumcampusapp.component.ui.overview.card.Card;
+import de.tum.in.tumcampusapp.component.ui.overview.card.CardViewHolder;
 import de.tum.in.tumcampusapp.utils.Const;
 import de.tum.in.tumcampusapp.utils.Utils;
 
@@ -20,10 +22,10 @@ import de.tum.in.tumcampusapp.utils.Utils;
 public class LoginPromtCard extends Card {
 
     public LoginPromtCard(Context context) {
-        super(CardManager.CARD_LOGIN, context, null);
+        super(CardManager.CARD_LOGIN, context, null, false);
     }
 
-    public static Card.CardViewHolder inflateViewHolder(ViewGroup parent) {
+    public static CardViewHolder inflateViewHolder(ViewGroup parent) {
         final View view = LayoutInflater.from(parent.getContext())
                                         .inflate(R.layout.card_login_promt, parent, false);
 
@@ -33,24 +35,24 @@ public class LoginPromtCard extends Card {
                 view.getContext().startActivity(loginIntent);
             });
 
-        return new Card.CardViewHolder(view);
+        return new CardViewHolder(view);
     }
 
     @Override
-    public void discard(SharedPreferences.Editor editor) {
-        Utils.setSetting(mContext, CardManager.SHOW_LOGIN, false);
+    public void discard(@NonNull SharedPreferences.Editor editor) {
+        Utils.setSetting(this.getContext(), CardManager.SHOW_LOGIN, false);
     }
 
     @Override
     protected boolean shouldShow(SharedPreferences p) {
         // show on top as long as user hasn't swiped it away and isn't connected to TUMonline
-        return Utils.getSettingBool(mContext, CardManager.SHOW_LOGIN, true)
-               && Utils.getSetting(mContext, Const.LRZ_ID, "").isEmpty();
+        return Utils.getSettingBool(this.getContext(), CardManager.SHOW_LOGIN, true)
+               && Utils.getSetting(this.getContext(), Const.LRZ_ID, "").isEmpty();
     }
 
     @Override
     public Intent getIntent() {
-        return new Intent(mContext, WizNavStartActivity.class);
+        return new Intent(this.getContext(), WizNavStartActivity.class);
     }
 
     @Override
