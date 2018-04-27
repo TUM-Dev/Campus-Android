f45a159b3b1847d1e2fb8b0d9f27637546252a1e
Jacqueline Bronger <jacqueline8711@gmx.de>
2018-04-27 10:39:05 +0200

    fix compile error

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/eduroam/EduroamCard.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/eduroam/EduroamCard.java
@@ -54,8 +54,8 @@ public class EduroamCard extends NotificationAwareCard {
     }
 
     private boolean eduroamAvailable(WifiManager wifi){
-        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             for(ScanResult scan: wifi.getScanResults()){
                 if(scan.SSID.equals(Const.EDUROAM_SSID)){
                     return true;
