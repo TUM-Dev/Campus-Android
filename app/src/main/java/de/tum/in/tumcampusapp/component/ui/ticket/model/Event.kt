0acf4b704ccbed225a9e023e382ce1c70e0b81de
Kailiangdong <kailiang.dong@tum.de>
2018-05-31 14:06:39 +0200

    Added Event model class

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/model/Event.kt
@@ -0,0 +1,28 @@
+package de.tum.`in`.tumcampusapp.component.ui.ticket.model
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+/**
+ * Events
+ *
+ * @param id      Event-ID
+ * @param image   Image url e.g. http://www.tu-film.de/img/film/poster/Fack%20ju%20Ghte.jpg
+ * @param title   Title
+ * @param description Description
+ * @param locality Locality
+ * @param date    Date
+ * @param link    Url, e.g. http://www.in.tum.de
+ */
+@Entity
+data class Event(@PrimaryKey
+                @SerializedName("event")
+                var id: Int = 0,
+                var image: String = "",
+                var title: String = "",
+                var description: String = "",
+                var locality: String = "",
+                var date: Date = Date(),
+                var link:  String = "")
