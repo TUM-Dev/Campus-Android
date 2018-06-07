f2fdb8342d9ed3071b2cc087cf728d74d88ff4c5
pfent <philipp@fent.de>
2018-06-07 23:03:01 +0200

    disable crashlytics for debug and -dev builds this fixes #824

--- app/src/main/java/de/tum/in/tumcampusapp/App.java
+++ app/src/main/java/de/tum/in/tumcampusapp/App.java
@@ -1,17 +1,26 @@
 package de.tum.in.tumcampusapp;
-import android.app.Application;
-import android.util.Log;
 
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
+
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        setupPicasso();
+        setupCrashlytics();
+    }
+
+    private void setupPicasso() {
         Picasso.Builder builder = new Picasso.Builder(this);
-        builder.downloader(new OkHttp3Downloader(this,Integer.MAX_VALUE));
+        builder.downloader(new OkHttp3Downloader(this, Integer.MAX_VALUE));
 
         Picasso built = builder.build();
         built.setLoggingEnabled(true);
@@ -23,4 +32,16 @@ public class App extends Application {
         Picasso.setSingletonInstance(built);
     }
 
+    /**
+     * Disable crashlytics for debug builds, or non-release builds indicated by the "dev" suffix
+     */
+    private void setupCrashlytics() {
+
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG || BuildConfig.VERSION_NAME.contains("dev"))
+                                                   .build())
+                .build();
+
+        Fabric.with(this, crashlyticsKit);
+    }
 }
