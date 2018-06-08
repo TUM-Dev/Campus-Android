5393d32a4005d71fb598fad1b179226874887662
Till Hellmund <thellmund@gmail.com>
2018-06-08 16:51:36 +0200

    Update content of tuition fees notification

--- app/src/main/java/de/tum/in/tumcampusapp/component/other/notifications/TuitionFeesNotificationsProvider.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/other/notifications/TuitionFeesNotificationsProvider.kt
@@ -2,8 +2,6 @@ package de.tum.`in`.tumcampusapp.component.other.notifications
 
 import android.app.PendingIntent
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.support.v4.app.NotificationCompat
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.component.other.notifications.model.AppNotification
 import de.tum.`in`.tumcampusapp.component.other.notifications.model.InstantNotification
@@ -15,24 +13,21 @@ class TuitionFeesNotificationsProvider(context: Context,
                                        private val tuition: Tuition) : NotificationsProvider(context) {
 
     override fun getNotifications(): List<AppNotification> {
-        val contentText = when (tuition.isReregistered) {
-            true -> String.format(context.getString(R.string.reregister_success), tuition.semesterBez)
-            false -> {
-                val amount = "${tuition.soll} €\n"
-                amount + String.format(context.getString(R.string.reregister_todo), tuition.frist)
-            }
+        if (tuition.isReregistered) {
+            return emptyList()
         }
 
-        notificationBuilder
-                .setContentText(contentText)
+        val title = context.getString(R.string.tuition_fees)
+        val text = String.format(context.getString(R.string.reregister_todo), tuition.frist)
+
+        val notification = notificationBuilder
+                .setContentTitle(title)
+                .setContentText(text)
                 .setSmallIcon(R.drawable.ic_notification)
                 .setLargeIcon(Utils.getLargeIcon(context, R.drawable.ic_money))
+                .build()
 
-        val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.wear_tuition_fee)
-        notificationBuilder.extend(NotificationCompat.WearableExtender().setBackground(bitmap))
-        val notification = notificationBuilder.build()
-
-        // TODO: Schedule notifications for 1 month, 2 weeks, 1 week, 3 days
+        // TODO: Intelligently schedule notifications for 1 month, 2 weeks, 1 week, 3 days
         val futureNotification = InstantNotification(AppNotification.TUITION_FEES_ID, notification)
 
         val intent = tuition.getIntent(context)
