510d254a7ff9f44db9b3bc73056a4c8e8f17f99c
Alexander Schulz <alexanderschulz94@gmx.de>
2018-06-10 18:40:15 +0200

    Basic Payment type selection with seperate site for stripe to enter card information

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/StripePaymentActivity.java
@@ -0,0 +1,19 @@
+package de.tum.in.tumcampusapp.component.ui.ticket;
+
+import android.os.Bundle;
+
+import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.component.other.generic.activity.BaseActivity;
+
+public class StripePaymentActivity extends BaseActivity {
+
+    public StripePaymentActivity() {
+        super(R.layout.activity_payment_stripe);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/TicketPaymentSelectActivity.java
@@ -0,0 +1,35 @@
+package de.tum.in.tumcampusapp.component.ui.ticket;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.component.other.generic.activity.BaseActivity;
+
+
+public class TicketPaymentSelectActivity extends BaseActivity {
+
+
+    public TicketPaymentSelectActivity() {
+        super(R.layout.activity_payment_selection);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button stripeButton = findViewById(R.id.stripe_button);
+        stripeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StripePaymentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
+
+
