b7f5000f099a8310546fcc81b0ba174c7fde9075
pfent <philipp@fent.de>
2018-06-13 11:54:39 +0200

    move setupNotificationChannels() to App Notifications could get swallowed, if StartupActivity isn't run, which would manifest in a bunch of Notifications being generated at startup

--- app/src/main/java/de/tum/in/tumcampusapp/App.java
+++ app/src/main/java/de/tum/in/tumcampusapp/App.java
@@ -1,8 +1,11 @@
 package de.tum.in.tumcampusapp;
+
 import android.app.Application;
 
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
+
+import de.tum.in.tumcampusapp.component.other.notifications.model.AppNotificationsManager;
 
 public class App extends Application {
 
@@ -10,11 +13,12 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         this.initPicasso();
+        AppNotificationsManager.INSTANCE.setupNotificationChannels(this);
     }
 
     protected void initPicasso() {
         Picasso.Builder builder = new Picasso.Builder(this);
-        builder.downloader(new OkHttp3Downloader(this,Integer.MAX_VALUE));
+        builder.downloader(new OkHttp3Downloader(this, Integer.MAX_VALUE));
 
         Picasso built = builder.build();
         built.setLoggingEnabled(true);
--- app/src/main/java/de/tum/in/tumcampusapp/component/other/notifications/model/AppNotificationsManager.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/other/notifications/model/AppNotificationsManager.kt
@@ -1,6 +1,13 @@
 package de.tum.`in`.tumcampusapp.component.other.notifications.model
 
+import android.annotation.TargetApi
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.support.annotation.RequiresApi
+import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.api.tumonline.AccessTokenManager
 import de.tum.`in`.tumcampusapp.component.other.notifications.ProvidesNotifications
 import de.tum.`in`.tumcampusapp.component.tumui.calendar.CalendarController
@@ -8,6 +15,7 @@ import de.tum.`in`.tumcampusapp.component.tumui.tutionfees.TuitionFeeManager
 import de.tum.`in`.tumcampusapp.component.ui.cafeteria.controller.CafeteriaManager
 import de.tum.`in`.tumcampusapp.component.ui.news.NewsController
 import de.tum.`in`.tumcampusapp.component.ui.transportation.TransportController
+import de.tum.`in`.tumcampusapp.utils.Const
 
 object AppNotificationsManager {
 
@@ -27,5 +35,34 @@ object AppNotificationsManager {
 
     fun getEnabledProviders(context: Context) =
             getProviders(context).filter { it.hasNotificationsEnabled() }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun Context.buildNotificationChannel(id: String, nameRes: Int, importance: Int) =
+            NotificationChannel(id, getString(nameRes), importance)
+
+    /**
+     * Set up NotificationChannels for Android O
+     */
+    @TargetApi(Build.VERSION_CODES.O)
+    fun setupNotificationChannels(c: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            (c.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?)?.apply {
+                listOf(
+                        c.buildNotificationChannel(Const.NOTIFICATION_CHANNEL_DEFAULT, R.string.channel_general, NotificationManager.IMPORTANCE_DEFAULT),
+                        c.buildNotificationChannel(Const.NOTIFICATION_CHANNEL_CHAT, R.string.channel_chat, NotificationManager.IMPORTANCE_DEFAULT)
+                                .apply { enableLights(true) },
+                        c.buildNotificationChannel(Const.NOTIFICATION_CHANNEL_EDUROAM, R.string.eduroam, NotificationManager.IMPORTANCE_LOW),
+                        c.buildNotificationChannel(Const.NOTIFICATION_CHANNEL_CAFETERIA, R.string.channel_cafeteria, NotificationManager.IMPORTANCE_LOW),
+                        c.buildNotificationChannel(Const.NOTIFICATION_CHANNEL_MVV, R.string.channel_mvv, NotificationManager.IMPORTANCE_LOW),
+                        c.buildNotificationChannel(Const.NOTIFICATION_CHANNEL_EMERGENCY, R.string.channel_tum_alarmierung, NotificationManager.IMPORTANCE_HIGH)
+                                .apply {
+                                    enableVibration(true)
+                                    enableLights(true)
+                                    lightColor = Color.RED
+                                }
+                ).forEach { createNotificationChannel(it) }
+            }
+        }
+    }
 
 }
