2b346cef716e6dd7dba6f03ccdca038612caf1cf
Till Hellmund <thellmund@gmail.com>
2018-06-26 10:44:53 +0200

    Improve ViewHolder initialization

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/grades/ExamListAdapter.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/grades/ExamListAdapter.java
@@ -46,22 +46,14 @@ public class ExamListAdapter extends SimpleStickyListHeadersAdapter<Exam> {
         ViewHolder holder;
         View view = convertView;
 
-        // find and init UI
         if (view == null) {
             view = mInflater.inflate(R.layout.activity_grades_listview, parent, false);
-            holder = new ViewHolder();
-            holder.tvName = view.findViewById(R.id.courseNameTextView);
-            holder.tvGrade = view.findViewById(R.id.gradeTextView);
-            holder.tvDetails1 = view.findViewById(R.id.examDateTextView);
-            holder.tvDetails2 = view.findViewById(R.id.examinerTextView);
-            holder.gradeBackground = context.getResources()
-                                            .getDrawable(R.drawable.grade_background);
-            holder.tvGrade.setBackground(holder.gradeBackground);
+            holder = new ViewHolder(view);
             view.setTag(holder);
         } else {
             holder = (ViewHolder) view.getTag();
         }
-        // fill UI with data
+
         Exam exam = infoList.get(position);
         if (exam != null) {
             holder.tvName.setText(exam.getCourse());
@@ -71,19 +63,28 @@ public class ExamListAdapter extends SimpleStickyListHeadersAdapter<Exam> {
             holder.tvDetails1.setText(String.format("%s: %s", context.getString(R.string.date), DF.format(exam.getDate())));
 
             holder.tvDetails2.setText(String.format("%s: %s, " +
-                                                    "%s: %s",
-                                                    context.getString(R.string.examiner), exam.getExaminer(),
-                                                    context.getString(R.string.mode), exam.getModus()));
+                            "%s: %s",
+                    context.getString(R.string.examiner), exam.getExaminer(),
+                    context.getString(R.string.mode), exam.getModus()));
         }
 
         return view;
     }
 
     static class ViewHolder {
+
+        TextView tvName;
+        TextView tvGrade;
         TextView tvDetails1;
         TextView tvDetails2;
-        TextView tvGrade;
-        TextView tvName;
         Drawable gradeBackground;
+
+        public ViewHolder(View itemView) {
+            tvName = itemView.findViewById(R.id.courseNameTextView);
+            tvGrade = itemView.findViewById(R.id.gradeTextView);
+            tvDetails1 = itemView.findViewById(R.id.examDateTextView);
+            gradeBackground = tvGrade.getBackground();
+        }
+
     }
 }
