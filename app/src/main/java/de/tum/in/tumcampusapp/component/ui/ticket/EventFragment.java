81d097b8e507f56b8b5733987e4bc571a74fa3f2
Kailiang Dong <38508002+Kailiangdong@users.noreply.github.com>
2018-06-26 15:35:09 +0200

    Improve ticket ui (#912)
    * fix cut off image placeholder in drawer head (#876)
    
    * fix cut off image placeholder in drawer head
    
    * Change text, so it fits into one line
    
    * Fix bug that occasionally caused transport search results to be empty (#882)
    
    It occurred because the API occasionally does not return the "quality" property of a search result, resulting in a JSONException in the app.
    
    * Fix crash in TransportDetails (#881)
    
    The crash occurs when a subway departure does not contain the line
    number. This seems to occur randomly at some transport stations, e.g.
    Marienplatz.
    
    * disable crashlytics for debug and -dev builds (#867)
    
    * disable crashlytics for debug and -dev builds
    this fixes #824
    
    * Simplify disabling crashlytics.
    
    * Add ConnectivityInterceptor to throw an exception if device is offline
    
    The Interceptor throws a NoNetworkConnectionException, which is a subclass of IOException. You can react to it in Retrofit's onFailure callback.
    
    * Add placeholder for news source icons
    
    Before, the Facebook icon was set as the standard news source icon and was replaced when the actual icon was loaded. Now, we display a gray placeholder icon until the actual icon is loaded.
    
    * Update design of CreateEventActivity (#886)
    
    * Update design of CreateEventActivity
    
    - Update paddings
    - Update design of date and time selectors
    
    * Update design of CalendarDetailsFragment
    
    - Update paddings
    - Update buttons
    
    * Unify date formatting in CreateEventActivity and CalendarDetailsFragment
    
    * Fix keyboard style for event title text field
    
    * Make interface improvements
    
    - Add click indicator for date and time text views
    - Add hints to text fields
    
    * Improve code formatting
    
    * Make various user experience improvements
    
    - Add close dialog to prevent accidental data loss
    - Open keyboard automatically when creating new event
    - Remove empty navigation drawer
    - Disable save button if event title is empty
    
    * Add German translation for close dialog text
    
    * Hide keyboard when the user exits CreateEventActivity
    
    * Add animations to GradesActivity (#837)
    
    * add crossfade between chart types
    
    * some code refactoring
    
    * hide chart with animation + clearer code
    
    * animate arrow + fix average grade
    
    * different arrow animation
    
    * fix rotation bug
    
    * put paths in extra res values file
    
    * fixes issue with refresh animation and overlapping of fading views
    
    * add margin between cards
    
    * card are separated a bit from each other, add tab to seperate allevent and bookedevent, backbutton works in EventActivity
    
    * use EqualSpacingItemDecoration,set tab as chatroomactivity, delete unnnecessary comments
    
    * change ui of Eventsoverview according to newest suggestion, and add Swiperefresh to Eventoverview
    
    * commit for change according to lastest suggestion from Michael

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/EventFragment.java
@@ -0,0 +1,80 @@
+package de.tum.in.tumcampusapp.component.ui.ticket;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.component.other.generic.adapter.EqualSpacingItemDecoration;
+import de.tum.in.tumcampusapp.component.ui.ticket.model.Event;
+
+public class EventFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    private View view;
+    private String title;//String for tab title
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout mSwipeLayout;
+
+    public EventFragment() {
+    }
+
+    @SuppressLint("ValidFragment")
+    public EventFragment(String title) {
+        this.title = title;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.event_fragment, container, false);
+        setRecyclerView();
+        mSwipeLayout = view.findViewById(R.id.event_refresh);
+        mSwipeLayout.setOnRefreshListener(this);
+        return view;
+
+    }
+
+    private void setRecyclerView() {
+
+        recyclerView = view
+                .findViewById(R.id.activity_events_list_view);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView
+                .setLayoutManager(new LinearLayoutManager(getActivity()));
+        int spacing = Math.round(getResources().getDimension(R.dimen.material_card_view_padding));
+        recyclerView.addItemDecoration(new EqualSpacingItemDecoration(spacing));
+        List<Event> events;
+        if (title.equals(this.getString(R.string.booked_events))) {
+            events = EventsController.getBookedEvents();
+        } else {
+            events = EventsController.getEvents();
+        }
+        EventsAdapter adapter = new EventsAdapter(events);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onRefresh() {
+        //TODO: should be rewrote again after intergration of backend, the following is just for testing purpose.
+        List<Event> events;
+        if (title.equals(this.getString(R.string.booked_events))) {
+            events = EventsController.getBookedEvents();
+        } else {
+            events = EventsController.getEvents();
+        }
+        EventsAdapter adapter = new EventsAdapter(events);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        mSwipeLayout.setRefreshing(false);
+    }
+}
