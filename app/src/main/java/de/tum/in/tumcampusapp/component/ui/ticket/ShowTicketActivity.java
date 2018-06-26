02976f464e29d14c8747a24bab125b68778d47e1
Kailiang Dong <38508002+Kailiangdong@users.noreply.github.com>
2018-06-26 16:10:58 +0200

    Improve show ticket (#923)
    * fix cut off image placeholder in drawer head (#876)
    
    * fix cut off image placeholder in drawer head
    
    * Change text, so it fits into one line
    
    * Fix bug that occasionally caused transport search results to be empty (#882)
    
    It occurred because the API occasionally does not return the "quality" property of a search result, resulting in a JSONException in the app.
    
    * Fix crash in TransportDetails (#881)
    
    The crash occurs when a subway departure does not contain the line
    number. This seems to occur randomly at some transport stations, e.g.
    Marienplatz.
    
    * disable crashlytics for debug and -dev builds (#867)
    
    * disable crashlytics for debug and -dev builds
    this fixes #824
    
    * Simplify disabling crashlytics.
    
    * Add ConnectivityInterceptor to throw an exception if device is offline
    
    The Interceptor throws a NoNetworkConnectionException, which is a subclass of IOException. You can react to it in Retrofit's onFailure callback.
    
    * Add placeholder for news source icons
    
    Before, the Facebook icon was set as the standard news source icon and was replaced when the actual icon was loaded. Now, we display a gray placeholder icon until the actual icon is loaded.
    
    * Update design of CreateEventActivity (#886)
    
    * Update design of CreateEventActivity
    
    - Update paddings
    - Update design of date and time selectors
    
    * Update design of CalendarDetailsFragment
    
    - Update paddings
    - Update buttons
    
    * Unify date formatting in CreateEventActivity and CalendarDetailsFragment
    
    * Fix keyboard style for event title text field
    
    * Make interface improvements
    
    - Add click indicator for date and time text views
    - Add hints to text fields
    
    * Improve code formatting
    
    * Make various user experience improvements
    
    - Add close dialog to prevent accidental data loss
    - Open keyboard automatically when creating new event
    - Remove empty navigation drawer
    - Disable save button if event title is empty
    
    * Add German translation for close dialog text
    
    * Hide keyboard when the user exits CreateEventActivity
    
    * Add animations to GradesActivity (#837)
    
    * add crossfade between chart types
    
    * some code refactoring
    
    * hide chart with animation + clearer code
    
    * animate arrow + fix average grade
    
    * different arrow animation
    
    * fix rotation bug
    
    * put paths in extra res values file
    
    * fixes issue with refresh animation and overlapping of fading views
    
    * commit for saving
    
    * Bigger Title, remove headers,use same background with qrcode,link address to googlemap,format date,show ticket price, set brightness
    
    * some more improvements for the show ticket ui
    
    format price to "4,50 €" instead of "4.5"
    separate views for every information
    added redemption state

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/ShowTicketActivity.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/ShowTicketActivity.java
@@ -1,7 +1,13 @@
 package de.tum.in.tumcampusapp.component.ui.ticket;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,8 +26,8 @@ import de.tum.in.tumcampusapp.component.ui.ticket.model.Ticket;
 
 public class ShowTicketActivity extends BaseActivity {
 
-    private TextView eventDetailsTextView;
-    private ImageView ticketQrCode;
+    private TextView eventLocationTextView;
+    private ImageView ticketQrCodeImageView;
 
     public ShowTicketActivity() {
         super(R.layout.activity_show_ticket);
@@ -30,25 +36,62 @@ public class ShowTicketActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        eventDetailsTextView = (TextView) findViewById(R.id.eventdetail);
-        ticketQrCode = (ImageView) findViewById(R.id.ticket_qrcode);
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+        eventLocationTextView = findViewById(R.id.ticket_event_location);
+        TextView eventTitleTextView = findViewById(R.id.ticket_event_title);
+        TextView eventDateTimeTextView = findViewById(R.id.ticket_event_date_time);
+        TextView eventPriceTextView = findViewById(R.id.ticket_event_price);
+        TextView eventRedemptionStateTextView = findViewById(R.id.ticket_event_redemption_state);
+        ticketQrCodeImageView = findViewById(R.id.ticket_qr_code);
 
         int eventId = getIntent().getIntExtra("eventID", 0);
 
         //Get data from Api backend, now it is mock up data
         Ticket ticket = TicketsController.getTicketByEventId(eventId);
-        String dateString = new SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.GERMANY).
+        String timeString = new SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.GERMANY).
                 format(ticket.getEvent().getDate());
-        //load eventdetail
-        String eventdetail = ticket.getEvent().getTitle() +
-                "\n" + ticket.getEvent().getLocality() +
-                "\n" + dateString;
-        eventDetailsTextView.setText(eventdetail);
+        String[] time = timeString.split(" ");
+        //set date format to current locale setting
+        java.text.DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+        String dateString = dateFormat.format(ticket.getEvent().getDate());
+        //load event details
+        String String = ticket.getEvent().getTitle();
+        String eventLocationString = ticket.getEvent().getLocality();
+        String eventDateTimeString = dateString + " " + time[1];
+        String eventPriceString = String.format(Locale.GERMANY, "%.2f",
+                ticket.getType().getPrice()) + " €";
 
+        String redemptionStateString = this.getString(R.string.redeemed) + ": " +
+                (ticket.getRedeemed() ?  this.getString(R.string.yes) : this.getString(R.string.no));
+
+        eventTitleTextView.setText(String);
+        eventDateTimeTextView.setText(eventDateTimeString);
+        eventPriceTextView.setText(eventPriceString);
+        eventRedemptionStateTextView.setText(redemptionStateString);
+
+        //set location text underline to show it can link to google map
+        eventLocationTextView.setText(eventLocationString);
+        eventLocationTextView.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        eventLocationTextView.setOnClickListener(view -> showMap());
         String code = ticket.getCode();
         //create the qrcode using library  zxing
         createQRCode(code);
+        //set current screen brightness 100%
+        setWindowBrightness(WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL);
+    }
 
+    private void showMap() {
+        eventLocationTextView.setTextColor(Color.RED);
+        String map = "http://maps.google.co.in/maps?q=" + eventLocationTextView.getText();
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
+        startActivity(mapIntent);
+    }
+
+    private void setWindowBrightness(float brightness) {
+        Window window = getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.screenBrightness = brightness;
+        window.setAttributes(lp);
     }
 
     private void createQRCode(String text) {
@@ -57,7 +100,7 @@ public class ShowTicketActivity extends BaseActivity {
             BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-            ticketQrCode.setImageBitmap(bitmap);
+            ticketQrCodeImageView.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
         }
