102bdede35123a0aec749fb052e6cb9673e109c7
Philipp Fent <philipp@fent.de>
2018-06-28 10:30:17 +0200

    remove Curricula, since since we're overhauling them (#866)
    * remove Curricula, since since we're overhauling them
    see #841
    
    * remove 'study_plans' string

--- app/src/main/java/de/tum/in/tumcampusapp/api/app/TUMCabeClient.java
+++ app/src/main/java/de/tum/in/tumcampusapp/api/app/TUMCabeClient.java
@@ -32,7 +32,6 @@ import de.tum.in.tumcampusapp.component.ui.chat.model.ChatPublicKey;
 import de.tum.in.tumcampusapp.component.ui.chat.model.ChatRegistrationId;
 import de.tum.in.tumcampusapp.component.ui.chat.model.ChatRoom;
 import de.tum.in.tumcampusapp.component.ui.chat.model.ChatVerification;
-import de.tum.in.tumcampusapp.component.ui.curricula.model.Curriculum;
 import de.tum.in.tumcampusapp.component.ui.news.model.News;
 import de.tum.in.tumcampusapp.component.ui.news.model.NewsAlert;
 import de.tum.in.tumcampusapp.component.ui.news.model.NewsSources;
@@ -62,7 +61,6 @@ public final class TUMCabeClient {
     private static final String API_CHAT = "chat/";
     static final String API_CHAT_ROOMS = API_CHAT + "rooms/";
     static final String API_CHAT_MEMBERS = API_CHAT + "members/";
-    static final String API_CURRICULA = "curricula/";
     static final String API_NOTIFICATIONS = "notifications/";
     static final String API_LOCATIONS = "locations/";
     static final String API_DEVICE = "device/";
@@ -189,12 +187,6 @@ public final class TUMCabeClient {
 
     public FcmNotification getNotification(int notification) throws IOException {
         return service.getNotification(notification)
-                      .execute()
-                      .body();
-    }
-
-    public List<Curriculum> getAllCurriculas() throws IOException {
-        return service.getAllCurriculas()
                       .execute()
                       .body();
     }
