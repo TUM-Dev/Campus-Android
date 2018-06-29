e32625d476cce92aaf2750aabea7a43abf244597
Till Hellmund <thellmund@gmail.com>
2018-06-29 08:59:44 +0200

    Use TUMOnlineClient for person search

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/PersonSearchActivity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/PersonSearchActivity.kt
@@ -6,21 +6,22 @@ import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import de.tum.`in`.tumcampusapp.R
-import de.tum.`in`.tumcampusapp.api.tumonline.TUMOnlineConst
+import de.tum.`in`.tumcampusapp.api.tumonline.TUMOnlineClient
 import de.tum.`in`.tumcampusapp.component.other.general.RecentsDao
 import de.tum.`in`.tumcampusapp.component.other.general.model.Recent
 import de.tum.`in`.tumcampusapp.component.other.generic.activity.ActivityForSearchingTumOnline
 import de.tum.`in`.tumcampusapp.component.tumui.person.model.Person
 import de.tum.`in`.tumcampusapp.component.tumui.person.model.PersonList
 import de.tum.`in`.tumcampusapp.database.TcaDb
-import de.tum.`in`.tumcampusapp.utils.Const
 import kotlinx.android.synthetic.main.activity_person_search.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * Activity to search for employees.
  */
 class PersonSearchActivity : ActivityForSearchingTumOnline<PersonList>(
-        TUMOnlineConst.PERSON_SEARCH,
         R.layout.activity_person_search,
         PersonSearchSuggestionProvider.AUTHORITY, 3
 ), PersonSearchResultsItemListener {
@@ -64,30 +65,35 @@ class PersonSearchActivity : ActivityForSearchingTumOnline<PersonList>(
     }
 
     public override fun onStartSearch(query: String) {
-        requestHandler.setParameter(Const.PERSON_SEARCH_TUM_REQUEST_KEY, query)
-        requestFetch()
+        searchPerson(query)
+    }
+
+    private fun searchPerson(query: String) {
+        showLoadingStart()
+        TUMOnlineClient
+                .getInstance(this)
+                .searchPerson(query)
+                .enqueue(object : Callback<PersonList> {
+                    override fun onResponse(call: Call<PersonList>, response: Response<PersonList>) {
+                        val personList = response.body() ?: return
+                        handleDownloadSuccess(personList)
+                    }
+
+                    override fun onFailure(call: Call<PersonList>, t: Throwable) {
+                        handleDownloadError(t)
+                    }
+                })
     }
 
     private fun showPersonDetails(person: Person) {
-        // Store selected person ID in bundle to get in in StaffDetailst
-        val bundle = Bundle().apply {
-            putSerializable("personObject", person)
-        }
-
-        // Show detailed information in new activity
         val intent = Intent(this, PersonDetailsActivity::class.java).apply {
-            putExtras(bundle)
+            putExtra("personObject", person)
         }
         startActivity(intent)
     }
 
-    /**
-     * Handles the XML response from TUMOnline by de-serializing the information
-     * to model entities.
-     *
-     * @param response The de-serialized data from TUMOnline.
-     */
-    public override fun onLoadFinished(response: PersonList) {
+    private fun handleDownloadSuccess(response: PersonList) {
+        showLoadingEnded()
         recentsHeader.visibility = View.GONE
 
         if (response.persons.size == 1) {
