c3ea1e5eebd10e9dfe44d041760c968b81134b38
Kordian Bruck <k@bruck.me>
2018-07-02 20:44:37 +0200

    Remove db annotations from payload clases

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/payload/TicketReservationResponse.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/payload/TicketReservationResponse.kt
@@ -1,10 +1,9 @@
-package de.tum.`in`.tumcampusapp.component.ui.ticket.model
+package de.tum.`in`.tumcampusapp.component.ui.ticket.payload
 
 import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity
-data class TicketReservationResponse(@PrimaryKey
+
+data class TicketReservationResponse(
                   @SerializedName("ticket_history")
                   var ticketHistory: Int)
--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/payload/TicketSuccessResponse.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/payload/TicketSuccessResponse.kt
@@ -1,4 +1,4 @@
 package de.tum.`in`.tumcampusapp.component.ui.ticket.payload
 
 
-data class TicketReservation(var ticket_type: Int = 0)
+data class TicketSuccessResponse(var success: Boolean)
