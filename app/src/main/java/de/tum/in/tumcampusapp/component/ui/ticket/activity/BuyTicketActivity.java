d732a06f4f1822ece6a9a4bb5fd9ef759e9c99f3
Alexander Schulz <alexanderschulz94@gmx.de>
2018-07-10 15:18:59 +0200

    add moved files again

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/activity/BuyTicketActivity.java
@@ -0,0 +1,212 @@
+package de.tum.in.tumcampusapp.component.ui.ticket.activity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.api.app.TUMCabeClient;
+import de.tum.in.tumcampusapp.api.tumonline.AccessTokenManager;
+import de.tum.in.tumcampusapp.component.other.generic.activity.BaseActivity;
+import de.tum.in.tumcampusapp.component.ui.ticket.EventsController;
+import de.tum.in.tumcampusapp.component.ui.ticket.model.Event;
+import de.tum.in.tumcampusapp.component.ui.ticket.model.TicketType;
+import de.tum.in.tumcampusapp.component.ui.ticket.payload.TicketReservationResponse;
+import de.tum.in.tumcampusapp.utils.Const;
+import de.tum.in.tumcampusapp.utils.DateTimeUtils;
+import de.tum.in.tumcampusapp.utils.Utils;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BuyTicketActivity extends BaseActivity {
+
+    private EventsController eventsController;
+    private int eventId;
+
+    private Spinner ticketTypeSpinner;
+    private ProgressBar reservationProgressBar;
+    private Button paymentButton;
+
+    private List<TicketType> ticketTypes;
+
+    public BuyTicketActivity() {
+        super(R.layout.activity_buy_ticket);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        eventsController = new EventsController(this);
+
+        eventId = getIntent().getIntExtra("eventID", 0);
+
+        // get ticket type information from API
+        Thread thread = new Thread() {
+            public void run() {
+                ticketTypes = eventsController.getTicketTypesByEventId(eventId);
+            }
+        };
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // if ticketTypes could not be retrieved from server, e.g. due to network problems
+        if (ticketTypes == null) {
+            Utils.showToast(getApplicationContext(), R.string.no_network_connection);
+            // go back to event details
+            Intent intent = new Intent(getApplicationContext(), EventDetailsActivity.class);
+            intent.putExtra("event_id", eventId);
+            startActivity(intent);
+        } else {
+            initEventTextViews();
+
+            initializeTicketTypeSpinner();
+
+            reservationProgressBar = findViewById(R.id.ticket_reservation_progressbar);
+            reservationProgressBar.setVisibility(View.INVISIBLE);
+
+            paymentButton = findViewById(R.id.paymentbutton);
+            paymentButton.setOnClickListener(v -> {
+                // Check if user is logged in and name and LRZ ID are available (needed to create ChatVerification)
+                if (new AccessTokenManager(BuyTicketActivity.this).hasValidAccessToken() &&
+                        Utils.getSetting(BuyTicketActivity.this, Const.LRZ_ID, "").length() > 0 &&
+                        Utils.getSetting(BuyTicketActivity.this, Const.CHAT_ROOM_DISPLAY_NAME, "").length() > 0) {
+                    reserveTicket();
+                } else {
+                    ContextThemeWrapper ctw = new ContextThemeWrapper(this, R.style.Theme_AppCompat_Light_Dialog_Alert);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
+                    builder.setTitle(getString(R.string.sorry))
+                            .setMessage(R.string.not_logged_in_message);
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            });
+        }
+    }
+
+    private void initEventTextViews() {
+        TextView eventView = findViewById(R.id.ticket_details_event);
+        TextView locationView = findViewById(R.id.ticket_details_location);
+        TextView dateView = findViewById(R.id.ticket_details_date);
+
+        Event event = eventsController.getEventById(eventId);
+
+        String eventString = event.getTitle();
+        String locationString = event.getLocality();
+
+        eventView.append(eventString);
+        locationView.append(locationString);
+        dateView.append(DateTimeUtils.INSTANCE.getDateTimeString(event.getStart()));
+    }
+
+    private void initializeTicketTypeSpinner() {
+        ticketTypeSpinner = findViewById(R.id.ticket_type_spinner);
+        ticketTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String ticketTypeName = (String) parent.getItemAtPosition(position);
+                setTicketTypeInformation(ticketTypeName);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // do nothing here for now
+            }
+        });
+
+        ArrayList<String> ticketTypeNames = new ArrayList<>();
+        for (TicketType ticketType : ticketTypes) {
+            ticketTypeNames.add(ticketType.getDescription());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, ticketTypeNames);
+        ticketTypeSpinner.setAdapter(adapter);
+    }
+
+    private TicketType getTicketTypeForName(String ticketTypeName) {
+        for (TicketType ticketType : ticketTypes) {
+            if (ticketType.getDescription().equals(ticketTypeName)) {
+                return ticketType;
+            }
+        }
+        return null;
+    }
+
+    private void setTicketTypeInformation(String ticketTypeName) {
+        TicketType ticketType = getTicketTypeForName(ticketTypeName);
+
+        TextView priceView = findViewById(R.id.ticket_details_price);
+
+        String priceString = ticketType.formatedPrice();
+
+        priceView.setText(priceString);
+    }
+
+    private void reserveTicket() {
+        TicketType ticketType = getTicketTypeForName((String) ticketTypeSpinner.getSelectedItem());
+        if (ticketType == null) {
+            Toast.makeText(getApplicationContext(), R.string.internal_error, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        reservationProgressBar.setVisibility(View.VISIBLE);
+        paymentButton.setEnabled(false);
+        try {
+            TUMCabeClient.getInstance(getApplicationContext()).reserveTicket(BuyTicketActivity.this, ticketType.getId(), new Callback<TicketReservationResponse>() {
+                @Override
+                public void onResponse(Call<TicketReservationResponse> call, Response<TicketReservationResponse> response) {
+                    if (response.body().getError() != null) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(BuyTicketActivity.this);
+                        builder.setTitle(getString(R.string.sorry))
+                                .setMessage(getString(R.string.ticket_contingent_exhausted));
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    } else {
+                        reservationProgressBar.setVisibility(View.INVISIBLE);
+                        paymentButton.setEnabled(true);
+
+                        // Jump to the payment activity
+                        Intent intent = new Intent(getApplicationContext(), StripePaymentActivity.class);
+                        intent.putExtra("ticketPrice", ticketType.formatedPrice());
+                        intent.putExtra("ticketType", ticketType.getId());
+                        intent.putExtra("ticketHistory", response.body().getTicketHistory());
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<TicketReservationResponse> call, Throwable t) {
+                    t.printStackTrace();
+                    reservationProgressBar.setVisibility(View.INVISIBLE);
+                    paymentButton.setEnabled(true);
+                    StripePaymentActivity.showError(BuyTicketActivity.this, getString(R.string.purchase_error_message));
+                }
+            });
+        } catch (IOException exception) {
+            reservationProgressBar.setVisibility(View.INVISIBLE);
+            paymentButton.setEnabled(true);
+            StripePaymentActivity.showError(BuyTicketActivity.this, getString(R.string.internal_error));
+        }
+    }
+
+
+}
+
---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/activity/StripePaymentActivity.java
@@ -0,0 +1,304 @@
+package de.tum.in.tumcampusapp.component.ui.ticket.activity;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatButton;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.stripe.android.CustomerSession;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.PaymentSession;
+import com.stripe.android.PaymentSessionConfig;
+import com.stripe.android.PaymentSessionData;
+import com.stripe.android.model.Source;
+import com.stripe.android.model.SourceCardData;
+import com.stripe.android.view.PaymentMethodsActivity;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.tum.in.tumcampusapp.R;
+import de.tum.in.tumcampusapp.api.app.TUMCabeClient;
+import de.tum.in.tumcampusapp.component.other.generic.activity.BaseActivity;
+import de.tum.in.tumcampusapp.component.ui.overview.MainActivity;
+import de.tum.in.tumcampusapp.component.ui.ticket.EventsController;
+import de.tum.in.tumcampusapp.component.ui.ticket.TicketEphemeralKeyProvider;
+import de.tum.in.tumcampusapp.component.ui.ticket.model.Ticket;
+import de.tum.in.tumcampusapp.component.ui.ticket.payload.TicketSuccessResponse;
+import de.tum.in.tumcampusapp.utils.Const;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class StripePaymentActivity extends BaseActivity {
+
+    ProgressDialog progressDialog;
+    Button savedCardsButton;
+    AppCompatButton buyButton;
+    ProgressBar progressBar;
+    EditText cardholderEditText;
+
+    PaymentSession paymentSession;
+    Boolean setSource = false;
+    int ticketHistory;
+    String price = ""; // ticket price is only used to display in textView -> String
+
+
+    public StripePaymentActivity() {
+        super(R.layout.activity_payment_stripe);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        price = getIntent().getStringExtra("ticketPrice");
+        ticketHistory = getIntent().getIntExtra("ticketHistory", -1);
+
+        if (ticketHistory < 0 || price.length() == 0) {
+            Toast.makeText(getApplicationContext(), R.string.internal_error, Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        initSubviews();
+        initStripeSession();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (paymentSession != null) {
+            paymentSession.onDestroy();
+        }
+        try {
+            //TODO: react to cancelation?
+            TUMCabeClient.getInstance(getApplicationContext()).cancelTicketReservation(StripePaymentActivity.this, ticketHistory, new Callback<TicketSuccessResponse>() {
+                @Override
+                public void onResponse(Call<TicketSuccessResponse> call, Response<TicketSuccessResponse> response) {
+                    System.out.println("Cancalation Success!");
+                }
+
+                @Override
+                public void onFailure(Call<TicketSuccessResponse> call, Throwable t) {
+                    System.out.println("Cancalation Error!");
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void initSubviews() {
+        buyButton = findViewById(R.id.complete_purchase_button);
+        buyButton.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             requestTicket();
+                                         }
+                                     }
+        );
+        buyButton.setEnabled(false); // disabled until customer- and paymentsession have been loaded
+
+        cardholderEditText = findViewById(R.id.cardholder_edit_text);
+
+        progressBar = findViewById(R.id.stripe_purchase_progress);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        savedCardsButton = findViewById(R.id.saved_cards_button);
+        savedCardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paymentSession.presentPaymentMethodSelection();
+            }
+        });
+        savedCardsButton.setEnabled(false);
+
+        // Insert formated string to remind users about which amount they are going to pay
+        TextView priceReminder = findViewById(R.id.payment_info_price_textview);
+        MessageFormat fmt = new MessageFormat(getString(R.string.payment_info_price_reminder));
+        Object[] args = {price};
+        priceReminder.setText(fmt.format(args));
+    }
+
+
+    private void requestTicket() {
+        String cardholder = cardholderEditText.getText().toString();
+        if (cardholder.length() == 0) {
+            Toast.makeText(getApplicationContext(), R.string.empty_cardholder_message, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!setSource) {
+            // No payment source selected yet
+            Toast.makeText(getApplicationContext(), R.string.card_data_invalid, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        setPurchaseRequestLoading();
+        try {
+            TUMCabeClient
+                    .getInstance(StripePaymentActivity.this)
+                    .purchaseTicketStripe(StripePaymentActivity.this, ticketHistory,
+                            paymentSession.getPaymentSessionData().getSelectedPaymentMethodId(),
+                            cardholder, new Callback<Ticket>() {
+                                @Override
+                                public void onResponse(Call<Ticket> call, Response<Ticket> response) {
+                                    EventsController ec = new EventsController(StripePaymentActivity.this);
+                                    List<Ticket> ticketList = new ArrayList<>();
+                                    ticketList.add(response.body());
+                                    ec.addTickets(ticketList);
+                                    finishLoadingPurchaseRequestSuccess(response.body());
+                                }
+
+                                @Override
+                                public void onFailure(Call<Ticket> call, Throwable t) {
+                                    t.printStackTrace();
+                                    finishLoadingPurchaseRequestError(getString(R.string.ticket_retrieval_error));
+                                    finish();
+                                }
+                            });
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            finishLoadingPurchaseRequestError(getString(R.string.purchase_error_message));
+            return;
+        }
+    }
+
+
+    private void setPurchaseRequestLoading() {
+        progressDialog = ProgressDialog.show(StripePaymentActivity.this, "", getString(R.string.purchase_progress_message), true);
+    }
+
+
+    private void finishLoadingPurchaseRequestSuccess(Ticket ticket) {
+        progressDialog.dismiss();
+
+        // Show success message to user
+        ContextThemeWrapper ctw = new ContextThemeWrapper(this, R.style.Theme_AppCompat_Light_Dialog_Alert);
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
+        builder.setTitle(getString(R.string.purchase_success_title))
+                .setMessage(getString(R.string.purchase_success_message))
+                .setPositiveButton(getString(R.string.purchase_success_continue), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(StripePaymentActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        intent = new Intent(StripePaymentActivity.this, ShowTicketActivity.class);
+                        intent.putExtra("eventID", ticket.getEventId());
+                        startActivity(intent);
+                    }
+                })
+                .setCancelable(false);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+    private void finishLoadingPurchaseRequestError(String error) {
+        progressDialog.dismiss();
+        StripePaymentActivity.showError(this, error);
+    }
+
+
+    public static void showError(Context context, String message) {
+        ContextThemeWrapper ctw = new ContextThemeWrapper(context, R.style.Theme_AppCompat_Light_Dialog_Alert);
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
+        builder.setTitle(context.getString(R.string.error))
+                .setMessage(message);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+    /* On return of the credit card selection */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Data might be null if user tapped Back button from Credit Card selection
+        if (data != null) {
+            String selectedSource = data.getStringExtra(PaymentMethodsActivity.EXTRA_SELECTED_PAYMENT);
+            Source source = Source.fromString(selectedSource);
+            // Note: it isn't possible for a null or non-card source to be returned.
+            if (source != null && Source.CARD.equals(source.getType())) {
+                SourceCardData cardData = (SourceCardData) source.getSourceTypeModel();
+                savedCardsButton.setText(buildCardString(cardData));
+                setSource = true;
+            }
+        }
+    }
+
+
+    private void initStripeSession() {
+        PaymentConfiguration.init(Const.STRIPE_API_PUBLISHABLE_KEY);
+        initCustomerSession();
+    }
+
+
+    private void initCustomerSession() {
+        CustomerSession.initCustomerSession(new TicketEphemeralKeyProvider(new TicketEphemeralKeyProvider.ProgressListener() {
+            @Override
+            public void onStringResponse(String string) {
+                if (string.startsWith("Error: ")) {
+                    StripePaymentActivity.showError(StripePaymentActivity.this, string);
+                } else {
+                    initPaymentSession();
+                }
+            }
+        }, getApplicationContext()));
+    }
+
+
+    private void initPaymentSession() {
+        paymentSession = new PaymentSession(this);
+        paymentSession.init(new PaymentSession.PaymentSessionListener() {
+
+            @Override
+            public void onCommunicatingStateChanged(boolean isCommunicating) {
+                // Show network activity to user
+                if (isCommunicating) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onError(int errorCode, @Nullable String errorMessage) {
+                System.out.println("Error: " + errorMessage);
+                StripePaymentActivity.showError(StripePaymentActivity.this, getString(R.string.customersession_init_failed));
+            }
+
+            @Override
+            public void onPaymentSessionDataChanged(@NonNull PaymentSessionData data) {
+                buyButton.setEnabled(true);
+                savedCardsButton.setEnabled(true);
+            }
+
+        }, new PaymentSessionConfig.Builder()
+                .setShippingMethodsRequired(false)
+                .setShippingInfoRequired(false)
+                .build());
+    }
+
+    private String buildCardString(@NonNull SourceCardData data) {
+        return data.getBrand() + ",  " + getString(R.string.creditcard_ending_in) + "  " + data.getLast4();
+    }
+
+}
