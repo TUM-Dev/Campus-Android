2b98fd5f688660157bf94be85ac18c82f46765b2
Alexander Schulz <30290755+schulzalexander@users.noreply.github.com>
2018-07-10 10:48:56 +0200

    As/949 remove email from stripe request (#974)
    * remove email parameter from api calls
    
    * small additional change
    
    * feedback from PR

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/payload/EphimeralKey.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/payload/EphimeralKey.kt
@@ -3,7 +3,5 @@ package de.tum.`in`.tumcampusapp.component.ui.ticket.payload
 import com.google.gson.annotations.SerializedName
 
 
-data class EphimeralKey(@SerializedName("customer_mail")
-                        var customerMail: String = "",
-                        @SerializedName("api_version")
+data class EphimeralKey(@SerializedName("api_version")
                         var apiVersion: String = "")
