7bea9af305d4c1c38a362512619af9b84e6e495c
Michael Sailer <michael.sailer@tum.de>
2018-07-14 23:21:01 +0200

    added blank lines to highlight section comments

--- app/src/main/java/de/tum/in/tumcampusapp/api/app/TUMCabeAPIService.java
+++ app/src/main/java/de/tum/in/tumcampusapp/api/app/TUMCabeAPIService.java
@@ -217,6 +217,7 @@ public interface TUMCabeAPIService {
     // TICKET SALE
 
     // Getting Event information
+
     @GET(API_EVENTS + "list")
     Call<List<Event>> getEvents();
 
@@ -227,6 +228,7 @@ public interface TUMCabeAPIService {
     Call<List<Event>> searchEvents(@Path("searchTerm") String searchTerm);
 
     // Getting Ticket information
+
     @POST(API_EVENTS + API_TICKET + "my")
     Call<List<Ticket>> getTickets(@Body ChatVerification chatVerification);
 
@@ -234,6 +236,7 @@ public interface TUMCabeAPIService {
     Call<List<TicketType>> getTicketTypes(@Path("eventID") int eventID);
 
     // Ticket reservation
+
     @POST(API_EVENTS + API_TICKET + "reserve")
     Call<TicketReservationResponse> reserveTicket(@Body ChatVerification chatVerification);
 
@@ -241,6 +244,7 @@ public interface TUMCabeAPIService {
     Call<TicketSuccessResponse> cancelTicketReservation(@Body ChatVerification chatVerification);
 
     // Ticket purchase
+
     @POST(API_EVENTS + API_TICKET + "payment/stripe/purchase")
     Call<Ticket> purchaseTicketStripe(@Body ChatVerification chatVerification);
 
