a0284c07cdb65419db199b8019f5270eb3b7e2c8
Till Hellmund <thellmund@gmail.com>
2018-07-25 15:29:43 +0200

    Add Retrofit for cafeteria API calls

---
+++ app/src/main/java/de/tum/in/tumcampusapp/api/cafeteria/CafeteriaAPIService.kt
@@ -0,0 +1,15 @@
+package de.tum.`in`.tumcampusapp.api.cafeteria
+
+import de.tum.`in`.tumcampusapp.component.ui.cafeteria.model.CafeteriaResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface CafeteriaAPIService {
+
+    @GET("exportDB.php?mensa_id=all")
+    fun getMenus(
+            @Header("Cache-Control") cacheControl: String
+    ): Call<CafeteriaResponse>
+
+}
---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/model/CafeteriaResponse.kt
@@ -0,0 +1,6 @@
+package de.tum.`in`.tumcampusapp.component.ui.cafeteria.model
+
+data class CafeteriaResponse(
+        val menus: List<CafeteriaMenu>,
+        val sideDishes: List<CafeteriaMenu>
+)
