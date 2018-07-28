b2be2cc642152e790fb0bc1ad8122c52c67a67bf
Michael Sailer <michael.sailer@tum.de>
2018-07-28 18:49:44 +0200

    refactor ShowTicketActivity according to feedback

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/activity/ShowTicketActivity.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/activity/ShowTicketActivity.java
@@ -13,9 +13,12 @@ import android.widget.TextView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.Writer;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import org.joda.time.DateTime;
 
 import de.tum.in.tumcampusapp.R;
 import de.tum.in.tumcampusapp.component.other.generic.activity.BaseActivity;
@@ -23,13 +26,26 @@ import de.tum.in.tumcampusapp.component.ui.ticket.EventsController;
 import de.tum.in.tumcampusapp.component.ui.ticket.model.Event;
 import de.tum.in.tumcampusapp.component.ui.ticket.model.Ticket;
 import de.tum.in.tumcampusapp.component.ui.ticket.model.TicketType;
+import de.tum.in.tumcampusapp.utils.Utils;
 
 public class ShowTicketActivity extends BaseActivity {
 
     private TextView eventLocationTextView;
     private ImageView ticketQrCodeImageView;
+    private TextView eventTitleTextView;
+    private TextView eventDateTimeTextView;
+    private TextView eventPriceTextView;
+    private TextView eventRedemptionStateTextView;
 
-    private EventsController eventsController;
+    private Ticket ticket;
+    private Event event;
+    private TicketType ticketType;
+
+    private String eventTitle;
+    private String eventLocation;
+    private String eventDateTime;
+    private String eventPrice;
+    private String redemptionState;
 
     public ShowTicketActivity() {
         super(R.layout.activity_show_ticket);
@@ -39,47 +55,59 @@ public class ShowTicketActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+
+        initializeViews();
+        getTicketDataFromController();
+        setEventAndTicketDetails();
+        setViewData();
+        createQRCode(ticket.getCode());
+        setWindowBrightnessToFull();
+    }
+
+    private void initializeViews() {
         eventLocationTextView = findViewById(R.id.ticket_event_location);
-        TextView eventTitleTextView = findViewById(R.id.ticket_event_title);
-        TextView eventDateTimeTextView = findViewById(R.id.ticket_event_date_time);
-        TextView eventPriceTextView = findViewById(R.id.ticket_event_price);
-        TextView eventRedemptionStateTextView = findViewById(R.id.ticket_event_redemption_state);
+        eventTitleTextView = findViewById(R.id.ticket_event_title);
+        eventDateTimeTextView = findViewById(R.id.ticket_event_date_time);
+        eventPriceTextView = findViewById(R.id.ticket_event_price);
+        eventRedemptionStateTextView = findViewById(R.id.ticket_event_redemption_state);
         ticketQrCodeImageView = findViewById(R.id.ticket_qr_code);
+    }
 
-        eventsController = new EventsController(this);
-
+    private void getTicketDataFromController() {
+        EventsController eventsController = new EventsController(this);
         int eventId = getIntent().getIntExtra("eventID", 0);
 
-        Ticket ticket = eventsController.getTicketByEventId(eventId);
-        Event event = eventsController.getEventById(ticket.getEventId());
-        TicketType ticketType = eventsController.getTicketTypeById(ticket.getTicketTypeId());
+        ticket = eventsController.getTicketByEventId(eventId);
+        event = eventsController.getEventById(ticket.getEventId());
+        ticketType = eventsController.getTicketTypeById(ticket.getTicketTypeId());
+    }
 
-        //load event details
-        String String = event.getTitle();
-        String eventLocationString = event.getLocality();
-        String eventDateTimeString = Event.methods.getFormattedDateTime(this, event.getStart());
-        String eventPriceString = "";
+    private void setEventAndTicketDetails() {
+        eventTitle = event.getTitle();
+        eventLocation = event.getLocality();
+        eventDateTime = Event.methods.getFormattedDateTime(this, event.getStart());
+        eventPrice = "";
         if (ticketType != null) {
-            eventPriceString = ticketType.getFormattedPrice();
+            eventPrice = ticketType.getFormattedPrice();
         }
-        String redemptionStateString = this.getString(R.string.redeemed) + ": " +
-                (ticket.getRedemption() != null ? Event.methods.getFormattedDateTime(this,
-                        ticket.getRedemption()) : this.getString(R.string.no));
 
-        eventTitleTextView.setText(String);
-        eventDateTimeTextView.setText(eventDateTimeString);
-        eventPriceTextView.setText(eventPriceString);
-        eventRedemptionStateTextView.setText(redemptionStateString);
+        // set redemption string
+        DateTime redemptionDate = ticket.getRedemption();
+        String formattedDateTime = redemptionDate == null ? this.getString(R.string.no) :
+                Event.methods.getFormattedDateTime(this, redemptionDate);
+        redemptionState = this.getString(R.string.redeemed, formattedDateTime);
+    }
 
-        //set location text underline to show it can link to google map
-        eventLocationTextView.setText(eventLocationString);
+    private void setViewData() {
+        eventTitleTextView.setText(eventTitle);
+        eventDateTimeTextView.setText(eventDateTime);
+        eventPriceTextView.setText(eventPrice);
+        eventRedemptionStateTextView.setText(redemptionState);
+
+        eventLocationTextView.setText(eventLocation);
+        // underline location text to indicate that it is linked to google map
         eventLocationTextView.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
         eventLocationTextView.setOnClickListener(view -> showMap());
-        String code = ticket.getCode();
-        //create the qrcode using library  zxing
-        createQRCode(code);
-        //set current screen brightness 100%
-        setWindowBrightness(WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL);
     }
 
     private void showMap() {
@@ -89,22 +117,22 @@ public class ShowTicketActivity extends BaseActivity {
         startActivity(mapIntent);
     }
 
-    private void setWindowBrightness(float brightness) {
+    private void setWindowBrightnessToFull() {
         Window window = getWindow();
         WindowManager.LayoutParams lp = window.getAttributes();
-        lp.screenBrightness = brightness;
+        lp.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL;
         window.setAttributes(lp);
     }
 
     private void createQRCode(String text) {
-        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        Writer multiFormatWriter = new MultiFormatWriter();
         try {
             BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
             ticketQrCodeImageView.setImageBitmap(bitmap);
         } catch (WriterException e) {
-            e.printStackTrace();
+            Utils.log(e);
         }
     }
 }
