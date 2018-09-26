a69b8abb8080f2057ba9faa6d35d064c9b395e65
pfent <philipp@fent.de>
2018-09-26 12:49:08 +0200

    bring NetUtils to modern API

--- app/src/main/java/de/tum/in/tumcampusapp/utils/NetUtils.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/utils/NetUtils.kt
@@ -2,13 +2,29 @@ package de.tum.`in`.tumcampusapp.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkCapabilities.NET_CAPABILITY_VALIDATED
+import android.os.Build
 
 object NetUtils {
 
-    private fun getNetInfo(context: Context) : NetworkInfo? {
+    private fun NetworkCapabilities.hasConnectivity(): Boolean {
+        val capability = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) NET_CAPABILITY_INTERNET else NET_CAPABILITY_VALIDATED
+        return hasCapability(capability)
+    }
+
+    private inline fun anyConnectedNetwork(context: Context, condition: (NetworkCapabilities) -> Boolean): Boolean {
         val connectivityMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        return connectivityMgr?.activeNetworkInfo
+                ?: return false
+
+        return connectivityMgr.allNetworks.asSequence().filter {
+            connectivityMgr.getNetworkInfo(it)?.isConnected ?: false
+        }.mapNotNull {
+            connectivityMgr.getNetworkCapabilities(it)
+        }.any {
+            condition(it)
+        }
     }
 
     /**
@@ -18,19 +34,20 @@ object NetUtils {
      */
     @JvmStatic
     fun isConnected(context: Context): Boolean {
-        val netInfo = getNetInfo(context) ?: return false
-        return netInfo.isConnectedOrConnecting
+        return anyConnectedNetwork(context) { it.hasConnectivity() }
     }
 
     /**
      * Check if a network connection is available or can be available soon
      * and if the available connection is a wifi internet connection
      *
-     * @return if a WiFi connection is available
+     * @return if an unmetered connection (probably WiFi) is available
      */
     @JvmStatic
     fun isConnectedWifi(context: Context): Boolean {
-        val netInfo = getNetInfo(context) ?: return false
-        return netInfo.isConnectedOrConnecting && netInfo.type == ConnectivityManager.TYPE_WIFI
+        return anyConnectedNetwork(context) {
+            it.hasConnectivity()
+                    && it.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+        }
     }
 }
