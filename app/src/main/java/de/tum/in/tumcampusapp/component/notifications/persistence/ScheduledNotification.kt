4c01b1fe5713ffb02c5b56c4460bedaadcf60af5
pfent <philipp@fent.de>
2018-09-26 12:49:39 +0200

    don't warn about data classes default constructors

--- app/src/main/java/de/tum/in/tumcampusapp/component/notifications/persistence/ScheduledNotification.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/notifications/persistence/ScheduledNotification.kt
@@ -3,8 +3,10 @@ package de.tum.`in`.tumcampusapp.component.notifications.persistence
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.RoomWarnings
 
 @Entity(tableName = "scheduled_notifications")
+@SuppressWarnings(RoomWarnings.DEFAULT_CONSTRUCTOR)
 data class ScheduledNotification(
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0,
