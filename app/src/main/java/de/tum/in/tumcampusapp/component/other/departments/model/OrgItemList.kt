4f6b7e9c04df48f1115a7d691a2a72523945b39a
pfent <philipp@fent.de>
2018-09-26 13:46:48 +0200

    Remove OrgItems and associated

--- app/src/main/java/de/tum/in/tumcampusapp/component/other/departments/model/OrgItemList.kt
+++
@@ -1,8 +0,0 @@
-package de.tum.`in`.tumcampusapp.component.other.departments.model
-
-import com.tickaroo.tikxml.annotation.Element
-import com.tickaroo.tikxml.annotation.Xml
-
-@Xml(name = "rowset")
-@Deprecated("Not used anywhere. Remove.")
-data class OrgItemList(@Element var groups: List<OrgItem> = mutableListOf())
