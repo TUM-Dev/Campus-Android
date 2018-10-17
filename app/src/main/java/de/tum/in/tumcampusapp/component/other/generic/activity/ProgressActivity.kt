0e22e0c1a67b915e43ae11da870290f398ebab7c
Philipp Fent <philipp@fent.de>
2018-10-17 16:37:05 +0200

    Register NetworkCallbacks instead of deprecated CONNECTIVITY_ACTION broadcasts (#1106)
    * bump build tools and anko
    
    * use requestNetwork instead of CONNECTIVITY_ACTION in ProgressActivity
    
    * use requestNetwork instead of CONNECTIVITY_ACTION in MainActivity
    
    * use registerNetworkCallback() instead of requestNetwork()
    requestNetwork() actually changes the network state and we probably do
    not want to fiddle with the user's network settings just now.
    Also apparently Intents were delivered on the main thread, so
    onRefresh() / refreshCards() need to be called on the UI thread to not
    break existing behaviour

--- app/src/main/java/de/tum/in/tumcampusapp/component/other/generic/activity/ProgressActivity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/other/generic/activity/ProgressActivity.kt
@@ -1,11 +1,9 @@
 package de.tum.`in`.tumcampusapp.component.other.generic.activity
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.graphics.Color
-import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.support.design.button.MaterialButton
 import android.support.design.widget.Snackbar
@@ -19,9 +17,10 @@ import android.widget.TextView
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.api.tumonline.exception.*
 import de.tum.`in`.tumcampusapp.component.other.generic.viewstates.*
-import de.tum.`in`.tumcampusapp.utils.NetUtils
+import de.tum.`in`.tumcampusapp.utils.NetUtils.internetCapability
 import de.tum.`in`.tumcampusapp.utils.setImageResourceOrHide
 import de.tum.`in`.tumcampusapp.utils.setTextOrHide
+import org.jetbrains.anko.connectivityManager
 import java.net.UnknownHostException
 
 /**
@@ -69,11 +68,9 @@ abstract class ProgressActivity(
 
     private var registered: Boolean = false
 
-    private val connectivityChangeReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            if (NetUtils.isConnected(context)) {
-                onRefresh()
-            }
+    private val networkCallback: NetworkCallback = object : NetworkCallback() {
+        override fun onAvailable(network: Network?) {
+            runOnUiThread(this@ProgressActivity::onRefresh)
         }
     }
 
@@ -149,8 +146,10 @@ abstract class ProgressActivity(
             showError(NoInternetViewState())
         }
 
-        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-        registerReceiver(connectivityChangeReceiver, filter)
+        val request = NetworkRequest.Builder()
+                .addCapability(internetCapability)
+                .build()
+        connectivityManager.registerNetworkCallback(request, networkCallback)
         registered = true
     }
 
@@ -186,7 +185,7 @@ abstract class ProgressActivity(
      */
     protected fun showLoadingStart() {
         if (registered) {
-            unregisterReceiver(connectivityChangeReceiver)
+            connectivityManager.unregisterNetworkCallback(networkCallback)
             registered = false
         }
 
@@ -242,7 +241,7 @@ abstract class ProgressActivity(
     override fun onDestroy() {
         super.onDestroy()
         if (registered) {
-            unregisterReceiver(connectivityChangeReceiver)
+            connectivityManager.unregisterNetworkCallback(networkCallback)
             registered = false
         }
     }
