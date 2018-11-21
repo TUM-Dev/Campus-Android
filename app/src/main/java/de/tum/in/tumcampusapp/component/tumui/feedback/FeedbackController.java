b7be13542ba53b0a8d6283b70adc2cdc71a24d0e
Jacqueline Bronger <jacqueline8711@gmx.de>
2018-11-21 14:11:34 +0100

    fix checking email

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/feedback/FeedbackController.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/feedback/FeedbackController.java
@@ -19,6 +19,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.util.Strings;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -126,8 +128,8 @@ public class FeedbackController {
         stopListeningForLocation();
 
         if (feedback.getIncludeEmail()
-                && (lrzId == null || lrzId.isEmpty())
-                && !isEmailValid(activity.findViewById(R.id.feedback_custom_email))) {
+                && (Strings.isEmptyOrWhitespace(lrzId)
+                || !isEmailValid(activity.findViewById(R.id.feedback_custom_email)))) {
             return;
         }
 
