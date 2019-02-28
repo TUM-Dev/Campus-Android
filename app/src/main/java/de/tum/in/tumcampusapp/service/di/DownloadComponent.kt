4211d722a768d24d0d3e319cb3ef850c9e911bbd
Till Hellmund <thellmund@gmail.com>
2019-02-28 08:50:42 +0100

    Prevent repeated grades notifications

--- app/src/main/java/de/tum/in/tumcampusapp/service/di/DownloadComponent.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/service/di/DownloadComponent.kt
@@ -1,12 +1,14 @@
 package de.tum.`in`.tumcampusapp.service.di
 
 import dagger.Subcomponent
+import de.tum.`in`.tumcampusapp.component.tumui.grades.GradeNotificationDeleteReceiver
 import de.tum.`in`.tumcampusapp.service.DownloadService
 
 @Subcomponent(modules = [DownloadModule::class])
 interface DownloadComponent {
 
     fun inject(downloadService: DownloadService)
+    fun inject(gradeNotificationDeleteReceiver: GradeNotificationDeleteReceiver)
 
     @Subcomponent.Builder
     interface Builder {
