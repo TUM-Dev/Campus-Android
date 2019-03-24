da84f74a94985c8aef962ca99a7bd2846f06c4f5
Till Hellmund <thellmund@gmail.com>
2019-03-24 08:51:24 +0100

    Fix Parcel-related crash of FeedbackActivity

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/feedback/model/Feedback.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/feedback/model/Feedback.kt
@@ -1,10 +1,10 @@
 package de.tum.`in`.tumcampusapp.component.tumui.feedback.model
 
 import android.os.Build
-import android.os.Parcel
 import android.os.Parcelable
 import de.tum.`in`.tumcampusapp.BuildConfig
 import de.tum.`in`.tumcampusapp.utils.Const
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -18,6 +18,7 @@ import kotlin.collections.ArrayList
  * @param osVersion helpful info if bugs are submitted
  * @param appVersion helpful info if bugs are submitted
  */
+@Parcelize
 data class Feedback(
         val id: String = UUID.randomUUID().toString(),
         var topic: String = Const.FEEDBACK_TOPIC_GENERAL,
@@ -32,59 +33,4 @@ data class Feedback(
         var imageCount: Int = 0,
         @Transient // don't send this
         var picturePaths: List<String> = ArrayList()
-): Parcelable {
-
-    constructor(parcel: Parcel) : this() {
-        topic = parcel.readString()!!
-        message = parcel.readString()!!
-        email = parcel.readString()!!
-        includeEmail = parcel.readInt() == 1
-        includeLocation = parcel.readInt() == 1
-
-        val hasLocation = parcel.readByte() == 1.toByte()
-        if (hasLocation) {
-            latitude = parcel.readDouble()
-            longitude = parcel.readDouble()
-        }
-
-        imageCount = parcel.readInt()
-        parcel.readStringList(picturePaths)
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(topic)
-        parcel.writeString(message)
-        parcel.writeString(email)
-        parcel.writeInt(if (includeEmail) 1 else 0)
-        parcel.writeInt(if (includeLocation) 1 else 0)
-
-        if (latitude != null && longitude != null) {
-            parcel.writeByte(1)
-        } else {
-            parcel.writeByte(0)
-        }
-
-        latitude?.let {
-            parcel.writeDouble(it)
-        }
-
-        longitude?.let {
-            parcel.writeDouble(it)
-        }
-
-        parcel.writeInt(imageCount)
-        parcel.writeStringList(picturePaths)
-    }
-
-    override fun describeContents() = 0
-
-    companion object CREATOR : Parcelable.Creator<Feedback> {
-        override fun createFromParcel(parcel: Parcel): Feedback {
-            return Feedback(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Feedback?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
