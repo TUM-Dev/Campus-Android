a91a002198bf2ac005ceea1ec45153994aa4fdbb
Till Hellmund <thellmund@users.noreply.github.com>
2019-04-24 11:00:41 +0200

    Prevent repeat grade notifications (#1295)
    * Prevent repeated grades notifications
    
    * Adjust constant naming

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/grades/GradeNotificationDeleteReceiver.kt
@@ -0,0 +1,43 @@
+package de.tum.`in`.tumcampusapp.component.tumui.grades
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import de.tum.`in`.tumcampusapp.di.app
+import de.tum.`in`.tumcampusapp.service.di.DownloadModule
+import java.util.*
+import javax.inject.Inject
+
+/**
+ * Receives delete intents from grade notification and updates the [GradesStore] with the newly
+ * released grades. This helps to prevent repeated notifications.
+ */
+class GradeNotificationDeleteReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var gradesStore: GradesStore
+
+    override fun onReceive(context: Context, intent: Intent) {
+        context.app.appComponent.downloadComponent()
+                .downloadModule(DownloadModule())
+                .build()
+                .inject(this)
+
+        val newGrades = intent.getStringArrayListExtra(KEY_NEW_GRADES)
+        val existingGrades = gradesStore.gradedCourses
+        val updatedGrades = existingGrades + newGrades
+        gradesStore.store(updatedGrades)
+    }
+
+    companion object {
+
+        private const val KEY_NEW_GRADES = "KEY_NEW_GRADES";
+
+        fun newIntent(context: Context, newGrades: List<String>): Intent {
+            return Intent(context, GradeNotificationDeleteReceiver::class.java)
+                    .putStringArrayListExtra(KEY_NEW_GRADES, newGrades as ArrayList<String>)
+        }
+
+    }
+
+}
--- app/src/main/java/de/tum/in/tumcampusapp/service/di/DownloadComponent.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/service/di/DownloadComponent.kt
@@ -1,6 +1,7 @@
 package de.tum.`in`.tumcampusapp.service.di
 
 import dagger.Subcomponent
+import de.tum.`in`.tumcampusapp.component.tumui.grades.GradeNotificationDeleteReceiver
 import de.tum.`in`.tumcampusapp.component.ui.onboarding.StartupActivity
 import de.tum.`in`.tumcampusapp.component.ui.ticket.activity.EventsActivity
 import de.tum.`in`.tumcampusapp.service.DownloadWorker
@@ -9,18 +10,14 @@ import de.tum.`in`.tumcampusapp.service.DownloadWorker
 interface DownloadComponent {
 
     fun inject(downloadWorker: DownloadWorker)
-
     fun inject(eventsActivity: EventsActivity)
-
     fun inject(startupActivity: StartupActivity)
+    fun inject(gradeNotificationDeleteReceiver: GradeNotificationDeleteReceiver)
 
     @Subcomponent.Builder
     interface Builder {
-
         fun downloadModule(downloadModule: DownloadModule): Builder
-
         fun build(): DownloadComponent
-
     }
 
 }
