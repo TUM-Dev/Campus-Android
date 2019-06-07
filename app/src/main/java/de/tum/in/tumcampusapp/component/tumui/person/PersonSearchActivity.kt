3d516d1a3345e9f239dc4eaad29b5f4bd747c0d2
Till Hellmund <thellmund@gmail.com>
2019-06-07 10:19:25 +0200

    Move content from PersonSearchActivity to PersonSearchFragment

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/PersonSearchActivity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/PersonSearchActivity.kt
@@ -1,94 +1,20 @@
 package de.tum.`in`.tumcampusapp.component.tumui.person
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import android.view.View
 import de.tum.`in`.tumcampusapp.R
-import de.tum.`in`.tumcampusapp.component.other.general.RecentsDao
-import de.tum.`in`.tumcampusapp.component.other.general.model.Recent
-import de.tum.`in`.tumcampusapp.component.other.generic.activity.ActivityForSearchingTumOnline
-import de.tum.`in`.tumcampusapp.component.tumui.person.model.Person
-import de.tum.`in`.tumcampusapp.component.tumui.person.model.PersonList
-import de.tum.`in`.tumcampusapp.database.TcaDb
-import kotlinx.android.synthetic.main.activity_person_search.*
+import de.tum.`in`.tumcampusapp.component.other.generic.activity.BaseActivity
 
-/**
- * Activity to search for employees.
- */
-class PersonSearchActivity : ActivityForSearchingTumOnline<PersonList>(
-        R.layout.activity_person_search,
-        PersonSearchSuggestionProvider.AUTHORITY, 3
-), PersonSearchResultsItemListener {
-
-    private lateinit var recentsDao: RecentsDao
-
-    private val recents: List<Person>
-        get() {
-            val recents = recentsDao.getAll(RecentsDao.PERSONS) ?: return emptyList()
-            return recents.map { recent -> Person.fromRecent(recent) }
-        }
+class PersonSearchActivity : BaseActivity(R.layout.activity_person_search) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        recentsDao = TcaDb.getInstance(this).recentsDao()
-
-        val layoutManager = LinearLayoutManager(this)
-
-        personsRecyclerView.setHasFixedSize(true)
-        personsRecyclerView.layoutManager = layoutManager
-
-        disableRefresh()
-
-        val adapter = PersonSearchResultsAdapter(recents, this)
-        if (adapter.itemCount == 0) {
-            openSearch()
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.contentFrame, PersonSearchFragment.newInstance())
+                .commit()
         }
-        personsRecyclerView.adapter = adapter
-
-        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
-        personsRecyclerView.addItemDecoration(itemDecoration)
-    }
-
-    override fun onItemSelected(person: Person) {
-        val lastSearch = person.id + "$" + person.getFullName().trim { it <= ' ' }
-        recentsDao.insert(Recent(lastSearch, RecentsDao.PERSONS))
-        showPersonDetails(person)
-    }
-
-    override fun onStartSearch() {
-        recentsHeader.visibility = View.VISIBLE
-        val adapter = personsRecyclerView.adapter as? PersonSearchResultsAdapter
-        adapter?.update(recents)
-    }
-
-    public override fun onStartSearch(query: String) {
-        searchPerson(query)
-    }
-
-    private fun searchPerson(query: String) {
-        val apiCall = apiClient.searchPerson(query)
-        fetch(apiCall)
-    }
-
-    override fun onDownloadSuccessful(response: PersonList) {
-        recentsHeader.visibility = View.GONE
-
-        if (response.persons.size == 1) {
-            showPersonDetails(response.persons.first())
-        } else {
-            val adapter = personsRecyclerView.adapter as? PersonSearchResultsAdapter
-            adapter?.update(response.persons)
-        }
-    }
-
-    private fun showPersonDetails(person: Person) {
-        val intent = Intent(this, PersonDetailsActivity::class.java).apply {
-            putExtra("personObject", person)
-        }
-        startActivity(intent)
     }
 
 }
--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/PersonSearchResultsAdapter.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/PersonSearchResultsAdapter.kt
@@ -1,16 +1,16 @@
 package de.tum.`in`.tumcampusapp.component.tumui.person
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.component.tumui.person.model.Person
-import kotlinx.android.synthetic.main.person_search_result_item.view.*
+import kotlinx.android.synthetic.main.person_search_result_item.view.textView
 
 class PersonSearchResultsAdapter(
         private var items: List<Person>,
-        private val listener: PersonSearchResultsItemListener
+        private val onItemClick: (Person) -> Unit
 ) : RecyclerView.Adapter<PersonSearchResultsAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,7 +20,7 @@ class PersonSearchResultsAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position], listener)
+        holder.bind(items[position], onItemClick)
     }
 
     override fun getItemCount() = items.size
@@ -32,9 +32,12 @@ class PersonSearchResultsAdapter(
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(person: Person, listener: PersonSearchResultsItemListener) = with(itemView) {
+        fun bind(
+            person: Person,
+            onItemClick: (Person) -> Unit
+        ) = with(itemView) {
             textView.text = person.getFullName()
-            setOnClickListener { listener.onItemSelected(person) }
+            setOnClickListener { onItemClick(person) }
         }
 
     }
