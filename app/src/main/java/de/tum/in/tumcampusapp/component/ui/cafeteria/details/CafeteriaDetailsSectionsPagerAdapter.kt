cabff1d0aa90ffd77b1b695aabcbbd52ad3145d5
Jacqueline Bronger <jacqueline8711@gmx.de>
2019-07-04 14:22:18 +0200

    convert CafeteriaDetails and OpenHoursHelper

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/details/CafeteriaDetailsSectionsPagerAdapter.kt
@@ -0,0 +1,41 @@
+package de.tum.`in`.tumcampusapp.component.ui.cafeteria.details
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * A [FragmentStatePagerAdapter] that returns a fragment corresponding to one
+ * of the sections/tabs/pages.
+ */
+class CafeteriaDetailsSectionsPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+    private var cafeteriaId: Int = 0
+    private var dates: List<DateTime> = ArrayList()
+    private val formatter: DateTimeFormatter = DateTimeFormat.fullDate()
+
+    fun setCafeteriaId(cafeteriaId: Int) {
+        this.cafeteriaId = cafeteriaId
+    }
+
+    fun update(menuDates: List<DateTime>) {
+        dates = menuDates
+        notifyDataSetChanged()
+    }
+
+    override fun getCount() = dates.size
+
+    override fun getItem(position: Int)
+        = CafeteriaDetailsSectionFragment.newInstance(cafeteriaId, dates[position])
+
+    override fun getPageTitle(position: Int)
+        = formatter.print(dates[position]).toUpperCase(Locale.getDefault())
+
+    override fun getItemPosition(`object`: Any): Int = PagerAdapter.POSITION_NONE
+
+}
