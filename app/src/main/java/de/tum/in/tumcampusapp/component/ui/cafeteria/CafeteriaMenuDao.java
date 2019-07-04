3c9a5420a53ca3db5a0e56e49dcf35587a1ae62e
Jacqueline Bronger <jacqueline8711@gmx.de>
2019-07-04 12:34:50 +0200

    convert Activity and Managers

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaMenuDao.java
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaMenuDao.java
@@ -1,14 +1,13 @@
 package de.tum.in.tumcampusapp.component.ui.cafeteria;
 
-import androidx.room.Dao;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-
 import org.joda.time.DateTime;
 
 import java.util.List;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 import de.tum.in.tumcampusapp.component.ui.cafeteria.model.CafeteriaMenu;
 import io.reactivex.Flowable;
 
@@ -19,7 +18,7 @@ public interface CafeteriaMenuDao {
     void removeCache();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insert(CafeteriaMenu... cafeteriaMenu);
+    void insert(List<CafeteriaMenu> cafeteriaMenus);
 
     @Query("SELECT strftime('%d-%m-%Y', date) FROM cafeteriaMenu " +
            "WHERE date > date('now','localtime') AND cafeteriaId=:cafeteriaId AND name=:dishName " +
