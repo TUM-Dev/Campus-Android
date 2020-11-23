028c68937c173f84af6fe43f8ba3b27f31a6c2ec
Simon Kreuzer <simon.kreuzer@tum.de>
2020-11-23 23:42:43 +0100

    darkened buttom sheet dialog
    and changed the icons colors a bit, again for fitting into the new concept

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/adapteritems/InformationContactItem.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/person/adapteritems/InformationContactItem.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import de.tum.`in`.tumcampusapp.R
 
-class InformationContactItem(text: String) : AbstractContactItem(R.string.additional_info, text, R.drawable.ic_action_info_black) {
+class InformationContactItem(text: String) : AbstractContactItem(R.string.additional_info, text, R.drawable.ic_action_info) {
 
     override fun getIntent(context: Context): Intent? = null
 }
