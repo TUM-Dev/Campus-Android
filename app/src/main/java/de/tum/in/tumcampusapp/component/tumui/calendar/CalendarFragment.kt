17697e925bd79f060e12651be860b0b654bb51e1
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2021-10-22 15:29:22 +0200

    Now correctly retrieves isWeekMode flag from shared preferences

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/calendar/CalendarFragment.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/calendar/CalendarFragment.kt
@@ -103,7 +103,11 @@ class CalendarFragment : FragmentForAccessingTumOnline<EventsResponse>(
         todayButton.setOnClickListener { weekView.goToToday() }
         showDate?.let { openEvent(eventId) }
 
-        isWeekMode = arguments?.getBoolean(Const.CALENDAR_WEEK_MODE) ?: false
+        // These are arguments passed into the calendarFragment on instantiation, figure out where this call comes from
+        // isWeekMode = arguments?.getBoolean(Const.CALENDAR_WEEK_MODE) ?: false
+        //TODO: Testing
+        //TODO: Fix deprecated code attached to this issue
+        isWeekMode = Utils.getSettingBool(requireContext(), Const.CALENDAR_WEEK_MODE, false)
 
         disableRefresh()
         loadEvents(CacheControl.USE_CACHE)
