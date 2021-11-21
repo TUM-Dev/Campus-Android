a6b36a8a2bea1b8f8d9fe94af51cdc819499ac12
Kordian Bruck <kordianbruck@users.noreply.github.com>
2021-11-21 15:22:30 +0100

    Generate gRPC backend client and add example how to call getTopNews (#1383)

---
+++ app/src/main/java/de/tum/in/tumcampusapp/api/app/BackendHelper.kt
@@ -0,0 +1,19 @@
+package de.tum.`in`.tumcampusapp.api.app
+
+import de.tum.`in`.tumcampusapp.api.backend.CampusApiProto
+import de.tum.`in`.tumcampusapp.api.backend.CampusGrpc
+import de.tum.`in`.tumcampusapp.api.backend.GetTopNewsRequest
+import io.grpc.ManagedChannelBuilder
+
+class BackendHelper {
+
+    // Dummy example code
+    public fun getBackendConnection() {
+        var managedChannel = ManagedChannelBuilder.forAddress("https://www.google.com", 443).build()
+        var blockingStub = CampusGrpc.newBlockingStub(managedChannel)
+
+        var newsReq = GetTopNewsRequest.newBuilder().build()
+        var topNews = blockingStub.getTopNews(newsReq)
+
+    }
+}
