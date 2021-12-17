2a9ec24387d05643d596bbab2b19aaee8845daa0
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2021-12-17 08:41:50 +0100

    Migrated NewsFragment.kt to view binding

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/news/NewsFragment.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/news/NewsFragment.kt
@@ -9,15 +9,16 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.api.tumonline.CacheControl.USE_CACHE
 import de.tum.`in`.tumcampusapp.component.other.generic.adapter.EqualSpacingItemDecoration
 import de.tum.`in`.tumcampusapp.component.other.generic.fragment.FragmentForDownloadingExternal
+import de.tum.`in`.tumcampusapp.databinding.FragmentNewsBinding
 import de.tum.`in`.tumcampusapp.di.injector
 import de.tum.`in`.tumcampusapp.service.DownloadWorker
 import de.tum.`in`.tumcampusapp.utils.NetUtils
 import de.tum.`in`.tumcampusapp.utils.Utils
-import kotlinx.android.synthetic.main.fragment_news.newsRecyclerView
 import java.lang.Math.round
 import javax.inject.Inject
 
@@ -32,10 +33,12 @@ class NewsFragment : FragmentForDownloadingExternal(
     @Inject
     lateinit var newsDownloadAction: DownloadWorker.Action
 
-    override val method: DownloadWorker.Action?
+    override val method: DownloadWorker.Action
         get() = newsDownloadAction
 
     private var firstVisibleItemPosition: Int? = null
+
+    private val binding by viewBinding(FragmentNewsBinding::bind)
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -55,7 +58,7 @@ class NewsFragment : FragmentForDownloadingExternal(
 
     private fun initRecyclerView() {
         val spacing = round(resources.getDimension(R.dimen.material_card_view_padding))
-        newsRecyclerView.addItemDecoration(EqualSpacingItemDecoration(spacing))
+        binding.newsRecyclerView.addItemDecoration(EqualSpacingItemDecoration(spacing))
     }
 
     override fun onStart() {
@@ -72,12 +75,15 @@ class NewsFragment : FragmentForDownloadingExternal(
             return
         }
 
-        val adapter = NewsAdapter(requireContext(), news)
-        newsRecyclerView.adapter = adapter
+        with(binding) {
+            val adapter = NewsAdapter(requireContext(), news)
+            newsRecyclerView.adapter = adapter
 
-        // Restore previous state (including selected item index and scroll position)
-        val firstVisiblePosition = firstVisibleItemPosition ?: newsController.todayIndex
-        newsRecyclerView.scrollToPosition(firstVisiblePosition)
+            // Restore previous state (including selected item index and scroll position)
+            val firstVisiblePosition = firstVisibleItemPosition ?: newsController.todayIndex
+            newsRecyclerView.scrollToPosition(firstVisiblePosition)
+        }
+
 
         showLoadingEnded()
     }
@@ -121,7 +127,7 @@ class NewsFragment : FragmentForDownloadingExternal(
             val key = "news_source_" + newsSources[index].id
             Utils.setSetting(requireContext(), key, isChecked)
 
-            val layoutManager = newsRecyclerView.layoutManager as LinearLayoutManager
+            val layoutManager = binding.newsRecyclerView.layoutManager as LinearLayoutManager
             firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
 
             requestDownload(USE_CACHE)
