336592567e41f22c0e8e4c1be1e5a4a233de7126
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2021-12-17 16:42:33 +0100

    Migrated PaymentConfirmationActivity.kt to view binding

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/activity/PaymentConfirmationActivity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/ticket/activity/PaymentConfirmationActivity.kt
@@ -6,16 +6,21 @@ import android.os.Bundle
 import android.view.MenuItem
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.component.other.generic.activity.BaseActivity
+import de.tum.`in`.tumcampusapp.databinding.ActivityPaymentConfirmationBinding
 import de.tum.`in`.tumcampusapp.utils.Const
-import kotlinx.android.synthetic.main.activity_payment_confirmation.*
 import java.util.*
 import kotlin.concurrent.schedule
 
 class PaymentConfirmationActivity : BaseActivity(R.layout.activity_payment_confirmation) {
 
+    private lateinit var binding: ActivityPaymentConfirmationBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityPaymentConfirmationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
         val eventId = intent.getIntExtra(Const.KEY_EVENT_ID, -1)
         val ticketAmount = intent.getIntExtra(Const.KEY_TICKET_AMOUNT, 1)
         if (eventId == -1) {
@@ -23,16 +28,16 @@ class PaymentConfirmationActivity : BaseActivity(R.layout.activity_payment_confi
             return
         }
 
-        bodyTextView.text = resources.getQuantityString(R.plurals.tickets_saved_in_app, ticketAmount)
-        showTicketButton.text = resources.getQuantityString(R.plurals.show_tickets, ticketAmount)
+        binding.bodyTextView.text = resources.getQuantityString(R.plurals.tickets_saved_in_app, ticketAmount)
+        binding.showTicketButton.text = resources.getQuantityString(R.plurals.show_tickets, ticketAmount)
 
-        showTicketButton.setOnClickListener {
+        binding.showTicketButton.setOnClickListener {
             val intent = Intent(this, ShowTicketActivity::class.java)
             intent.putExtra(Const.KEY_EVENT_ID, eventId)
             startActivity(intent)
         }
 
-        doneButton.setOnClickListener {
+        binding.doneButton.setOnClickListener {
             val intent = Intent(this, EventsActivity::class.java).apply {
                 addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             }
@@ -46,7 +51,7 @@ class PaymentConfirmationActivity : BaseActivity(R.layout.activity_payment_confi
     private fun runCheckmarkAnimation() {
         Timer().schedule(200) {
             runOnUiThread {
-                val animatedCheckmark = imageView.drawable as? Animatable
+                val animatedCheckmark = binding.imageView.drawable as? Animatable
                 animatedCheckmark?.start()
             }
         }
