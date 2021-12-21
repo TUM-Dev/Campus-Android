9ce40a23e3344299db656f85a50ca3453c9333e3
Jakob Foerste <github@jfoe.de>
2021-12-21 18:52:16 +0100

    fix double url encoding and cleaner coord-api error handling (#1411)

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/roomfinder/model/RoomFinderCoordinate.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/roomfinder/model/RoomFinderCoordinate.kt
@@ -3,5 +3,6 @@ package de.tum.`in`.tumcampusapp.component.tumui.roomfinder.model
 data class RoomFinderCoordinate(
     var utm_zone: String = "",
     var utm_easting: String = "",
-    var utm_northing: String = ""
+    var utm_northing: String = "",
+    var error: String = ""
 )
