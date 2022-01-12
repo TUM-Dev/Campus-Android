d39b7210be80548e8fdaac56cb3ffa65942d18f7
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2022-01-12 14:50:57 +0100

    Updated queries to work with new table; Added documentation

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaMenuDao.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaMenuDao.kt
@@ -23,10 +23,18 @@ interface CafeteriaMenuDao {
     @Query("SELECT strftime('%d-%m-%Y', date) FROM cafeteriaMenu " +
             "WHERE date > date('now','localtime') AND cafeteriaId=:cafeteriaId AND name=:dishName " +
             "ORDER BY date ASC")
-    fun getNextDatesForDish(cafeteriaId: Int, dishName: String): Flowable<List<String>>
+    fun getNextDatesForDish(cafeteriaId: String, dishName: String): Flowable<List<String>>
 
-    @Query("SELECT id, cafeteriaId, date, dishType, name FROM cafeteriaMenu " +
-            "WHERE cafeteriaId = :cafeteriaId AND date = :date " +
+    /**
+     * The strftime is used for both the stored and passed date to ensure both calls with "YY-MM-DD" and
+     * "YY-MM-DD hh:mm:ss" or other valid datetime formats will succeed in finding a dishes if there are any.
+     *
+     * @param cafeteriaId the cafeteria for which dishes should be fetched as string e.g.: "mensa-garching"
+     * @param date the date for which dishes should be fetched.
+     * @return Any CafeteriaMenu items matching the criterion specified by the parameters
+     */
+    @Query("SELECT id, cafeteriaId, date , dishType, name, labels, calendarWeek FROM cafeteriaMenu " +
+            "WHERE cafeteriaId = :cafeteriaId AND strftime('%Y-%m-%d', date) = strftime('%Y-%m-%d', :date) " +
             "GROUP BY dishType ORDER BY dishType DESC")
-    fun getCafeteriaMenus(cafeteriaId: Int, date: DateTime): List<CafeteriaMenu>
+    fun getCafeteriaMenus(cafeteriaId: String, date: DateTime): List<CafeteriaMenu>
 }
