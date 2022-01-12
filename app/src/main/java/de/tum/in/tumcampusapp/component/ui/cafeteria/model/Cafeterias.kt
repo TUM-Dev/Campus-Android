6399406c7f370ea4729f3ead02b2ab5151076abd
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2022-01-12 18:11:03 +0100

    Added an enum for the cafeteriaIds

---
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/model/Cafeterias.kt
@@ -0,0 +1,34 @@
+package de.tum.`in`.tumcampusapp.component.ui.cafeteria.model
+
+// TODO double check that these esp the one with two underscores and check spelling
+enum class Cafeterias {
+    STUCAFE_KARLSTR,
+    STUCAFE_BOLTZMANNSTR,
+    STUCAFE_GARCHING,
+    STUCAFE_PASING,
+    STUCAFE_AKADEMIE_WEIHENSTEPHAN,
+    STUCAFE_ADALBERTSTR,
+    STUBISTRO_SCHELLINGSTR,
+    STUBISTRO_ROSENHEIM,
+    STUBISTRO_ARCISSTR,
+    STUBISTRO_GROSSHADERN,
+    STUBISTRO_GOETHESTR,
+    FMI_BISTRO,
+    IPP_BISTRO,
+    MENSA_PASING,
+    MENSA_LOTHSTR,
+    MENSA_LEOPOLDSTR,
+    MENSA_GARCHING,
+    MENSA_ARCISSTR,
+    MENSA_MARTINSRIED,
+    MENSA_WEIHENSTEPHAN,
+    MEDIZINER_MENSA;
+
+    /**
+     * Used to get the actual raw string cafeteriaId stored in the database. This method should be called when interacting
+     * with a DAO method that requires a cafeteriaId as a parameter.
+     */
+    fun toIdString(): String {
+        return this.toString().replace('_','-').lowercase()
+    }
+}
