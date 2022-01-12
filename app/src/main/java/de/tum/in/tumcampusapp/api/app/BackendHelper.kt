ca22c5a20a3a7a866bfa77eaec376ce3896bc109
Joscha Henningsen <joscha.henningsen@tum.de>
2022-01-12 09:57:46 +0100

    add grpc client, fiddle with gradle settings

--- app/src/main/java/de/tum/in/tumcampusapp/api/app/BackendHelper.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/api/app/BackendHelper.kt
@@ -1,21 +1,15 @@
 package de.tum.`in`.tumcampusapp.api.app
 
-/*
-import de.tum.`in`.tumcampusapp.api.backend.CampusApiProto
-import de.tum.`in`.tumcampusapp.api.backend.CampusGrpc
-import de.tum.`in`.tumcampusapp.api.backend.GetTopNewsRequest
+
+import com.google.protobuf.Empty
+import de.tum.`in`.tumcampusapp.api.backend.*
 import io.grpc.ManagedChannelBuilder
-*/
+
 
 class BackendHelper {
 
-    /*// Dummy example code
     public fun getBackendConnection() {
         var managedChannel = ManagedChannelBuilder.forAddress("https://www.google.com", 443).build()
         var blockingStub = CampusGrpc.newBlockingStub(managedChannel)
-
-        var newsReq = GetTopNewsRequest.newBuilder().build()
-        var topNews = blockingStub.getTopNews(newsReq)
-
-    }*/
+    }
 }
