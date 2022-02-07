c34ca07ec5b5df805d07af5233696d5bd69e5345
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2022-02-07 17:22:35 +0100

    Refactored CafeteriaMenuDao to int cafeteriaId and new table field names; Added helper query getIdFrom(cafeteriaSlug: String):Int

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaMenuDao.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaMenuDao.kt
@@ -21,20 +21,26 @@ interface CafeteriaMenuDao {
     fun insert(cafeteriaMenus: List<CafeteriaMenu>)
 
     @Query("SELECT strftime('%d-%m-%Y', date) FROM cafeteriaMenu " +
-            "WHERE date > date('now','localtime') AND cafeteriaId=:cafeteriaId AND name=:dishName " +
+            "WHERE date > date('now','localtime') AND id = :cafeteriaId AND name = :dishName " +
             "ORDER BY date ASC")
     fun getNextDatesForDish(cafeteriaId: String, dishName: String): Flowable<List<String>>
 
     /**
-     * The strftime is used for both the stored and passed date to ensure both calls with "YY-MM-DD" and
-     * "YY-MM-DD hh:mm:ss" or other valid datetime formats will succeed in finding a dishes if there are any.
-     *
-     * @param cafeteriaId the cafeteria for which dishes should be fetched as string e.g.: "mensa-garching"
+     * @param cafeteriaId the cafeteria for which dishes should be fetched (auto generated integer primary key)
      * @param date the date for which dishes should be fetched.
      * @return Any CafeteriaMenu items matching the criterion specified by the parameters
      */
-    @Query("SELECT id, cafeteriaId, date , dishType, name, labels, calendarWeek FROM cafeteriaMenu " +
-            "WHERE cafeteriaId = :cafeteriaId AND strftime('%Y-%m-%d', date) = strftime('%Y-%m-%d', :date) " +
+    @Query("SELECT id, slug, date, dishType, name, labels, calendarWeek FROM cafeteriaMenu " +
+            "WHERE id = :cafeteriaId AND date = :date " +
             "GROUP BY dishType ORDER BY dishType DESC")
-    fun getCafeteriaMenus(cafeteriaId: String, date: DateTime): List<CafeteriaMenu>
+    fun getCafeteriaMenus(cafeteriaId: Int, date: DateTime): List<CafeteriaMenu>
+
+    /**
+     * Used for fetching the actual primary key 'id' from the table via the CafeteriaLocation enum and the cafeteriaId embedded in that enum.
+     *
+     * @param cafeteriaSlug the cafeteria for which dishes should be fetched as valid cafeteria slug e.g.: "mensa-garching"
+     * @return the 'id' corresponding to the provided cafeteriaId
+     */
+    @Query("SELECT id FROM cafeteriaMenu WHERE slug = :cafeteriaSlug")
+    fun getIdFrom(cafeteriaSlug: String): Int
 }
