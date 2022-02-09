ffd8b688b797cb0b0b81be1c50eaf663441d1c2b
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2022-02-09 12:57:15 +0100

    CafeteriaDownloadAction no longer fetches CafeteriaMenus

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaDownloadAction.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/CafeteriaDownloadAction.kt
@@ -1,18 +1,16 @@
 package de.tum.`in`.tumcampusapp.component.ui.cafeteria
 
 import de.tum.`in`.tumcampusapp.api.tumonline.CacheControl
-import de.tum.`in`.tumcampusapp.component.ui.cafeteria.controller.CafeteriaMenuManager
 import de.tum.`in`.tumcampusapp.component.ui.cafeteria.repository.CafeteriaRemoteRepository
 import de.tum.`in`.tumcampusapp.service.DownloadWorker
 import javax.inject.Inject
 
 class CafeteriaDownloadAction @Inject constructor(
-    private val cafeteriaMenuManager: CafeteriaMenuManager,
     private val cafeteriaRemoteRepository: CafeteriaRemoteRepository
 ) : DownloadWorker.Action {
 
     override fun execute(cacheBehaviour: CacheControl) {
-        cafeteriaMenuManager.downloadMenus(cacheBehaviour)
-        cafeteriaRemoteRepository.fetchCafeterias(cacheBehaviour == CacheControl.BYPASS_CACHE)
+        // TODO this cache behaviour might be bit weird. Set it to force while making changes to the DB.
+        cafeteriaRemoteRepository.fetchCafeterias(true)
     }
 }
