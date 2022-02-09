f06dfb754226c79cbe7d6573da39e5efcc53d238
Tobias Lindenbauer <tobias.lindenbauer@tum.de>
2022-02-09 13:02:51 +0100

    Removed old methods for handling cafeteria menu downloads via CafeteriaDownloadAction and unused DAO

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/controller/CafeteriaMenuManager.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/controller/CafeteriaMenuManager.kt
@@ -26,57 +26,12 @@ class CafeteriaMenuManager
 @Inject
 constructor(private val context: Context) {
     private val menuDao: CafeteriaMenuDao
-    private val cafeteriaDao: CafeteriaDao
     private val favoriteDishDao: FavoriteDishDao
 
     init {
         val db = TcaDb.getInstance(context)
         menuDao = db.cafeteriaMenuDao()
         favoriteDishDao = db.favoriteDishDao()
-        cafeteriaDao = db.cafeteriaDao()
-    }
-
-    /**
-     * Download cafeteria menus from external interface (JSON)
-     *
-     * @param cacheControl BYPASS_CACHE to force download over normal sync period, else false
-     */
-    fun downloadMenus(cacheControl: CacheControl) {
-        // Responses from the cafeteria API are cached for one day. If the download is forced,
-        // we add a "no-cache" header to the request.
-        CafeteriaAPIClient
-                .getInstance(context)
-                .getMenus(cacheControl, CafeteriaLocation.MENSA_GARCHING)
-                // TODO Implement lazy/dynamic menu fetching
-                //  Actually have to do this right away, as this is the only way of getting the cafeteriaId
-                //  If I dont know the id with which this fetching is called, then I have no way of getting the id at all
-                //      => breaks new DB
-                .enqueue(object : Callback<CafeteriaResponse> {
-                    override fun onResponse(
-                        call: Call<CafeteriaResponse>,
-                        response: Response<CafeteriaResponse>
-                    ) {
-                        val cafeteriaResponse = response.body()
-                        if (cafeteriaResponse != null) {
-                            onDownloadSuccess(cafeteriaResponse)
-                        } else {
-                             Utils.logWithTag(this.javaClass.name, "Error fetching cafeteria menus. 'cafeteriaResponse' was null.")
-                        }
-                    }
-
-                    override fun onFailure(call: Call<CafeteriaResponse>, t: Throwable) {
-                        Utils.log(t)
-                    }
-                })
-    }
-
-    private fun onDownloadSuccess(response: CafeteriaResponse) {
-        menuDao.removeCache()
-
-        val menusToInsert = EatAPIParser.parseCafeteriaMenuFrom(response, cafeteriaDao.getIdFrom(CafeteriaLocation.MENSA_GARCHING.toSlug()))
-        menuDao.insert(menusToInsert)
-
-        scheduleNotificationAlarms()
     }
 
     fun scheduleNotificationAlarms() {
