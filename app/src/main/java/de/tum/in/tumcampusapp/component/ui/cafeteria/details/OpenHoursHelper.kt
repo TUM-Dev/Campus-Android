fc8a5e0da6cebc96ea433aabba505b3de997a3c8
YujinKim <ge89jow@mytum.de>
2022-07-13 17:34:45 +0200

    Edit OpenHoursHelper.kt for readability by adding "info" variable(#1459)

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/details/OpenHoursHelper.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteria/details/OpenHoursHelper.kt
@@ -81,22 +81,27 @@ class OpenHoursHelper(private val context: Context) {
             // Check the relativity
             val relativeTo: DateTime
             val relation: Int
+            // To show more informations regarding time
+            var info = ""
+
             if (opens.isAfter(now)) {
                 relation = R.string.opens
                 relativeTo = opens
+                info = " (" + DateTimeUtils.getTimeString(opens) + " - " + DateTimeUtils.getTimeString(closes) + ")"
             } else if (closes.isAfter(now)) {
                 relation = R.string.closes
                 relativeTo = closes
+                info = " (" + context.getString(R.string.on) + " " + DateTimeUtils.getTimeString(closes) + ")"
             } else {
                 relation = R.string.closed
                 relativeTo = closes
             }
-
             // Get the relative string
             val relativeTime = DateTimeUtils.formatFutureTime(relativeTo, context)
             // Return an assembly
-            return context.getString(relation) + " " + relativeTime.substring(0, 1)
-                    .lowercase(Locale.getDefault()) + relativeTime.substring(1)
+            return context.getString(relation) + " " + relativeTime.substring(0, 1).lowercase(Locale.getDefault()) +
+                    relativeTime.substring(1) + info
+
         } else {
             // future --> show non-relative opening hours
             return context.getString(R.string.opening_hours) + ": " +
