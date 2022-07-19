e34999ef7320d6d352a22d39abb009190ec62997
Benjamin Pabst <benjamin@pabst.dev>
2022-07-19 15:28:58 +0200

    Optimized back-button solution, fixed bug #1482

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/barrierfree/BarrierFreeFacilitiesActivity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/barrierfree/BarrierFreeFacilitiesActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.NavUtils
 import de.tum.`in`.tumcampusapp.R
@@ -17,6 +20,7 @@ import de.tum.`in`.tumcampusapp.component.tumui.roomfinder.model.RoomFinderRoom
 import de.tum.`in`.tumcampusapp.database.TcaDb
 import de.tum.`in`.tumcampusapp.databinding.ActivityBarrierFreeFacilitiesBinding
 import retrofit2.Call
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView
 
 class BarrierFreeFacilitiesActivity : ActivityForAccessingTumCabe<List<RoomFinderRoom>>(
         R.layout.activity_barrier_free_facilities
@@ -30,31 +34,19 @@ class BarrierFreeFacilitiesActivity : ActivityForAccessingTumCabe<List<RoomFinde
         LocationManager(this)
     }
 
-    private lateinit var binding: ActivityBarrierFreeFacilitiesBinding
+    private lateinit var listView: StickyListHeadersListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityBarrierFreeFacilitiesBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.let {
-            val parent = NavUtils.getParentActivityName(this)
-            if (parent != null) {
-                it.setDisplayHomeAsUpEnabled(true)
-                it.setHomeButtonEnabled(true)
-            }
-        }
-
-        binding.spinnerToolbar.onItemSelectedListener = this
+        listView = findViewById(R.id.barrierFreeFacilitiesListView)
+        val spinnerToolbar = findViewById<Spinner>(R.id.spinnerToolbar)
+        spinnerToolbar.onItemSelectedListener = this
     }
 
     override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
         when (position) {
             0 -> fetchApiCallForCurrentLocation()
-            1 -> executeApiCall(apiClient.listOfElevators)
+            1 -> executeApiCall(apiClient.listOfToilets)
             else -> executeApiCall(apiClient.listOfElevators)
         }
     }
@@ -73,8 +65,8 @@ class BarrierFreeFacilitiesActivity : ActivityForAccessingTumCabe<List<RoomFinde
     }
 
     override fun onDownloadSuccessful(response: List<RoomFinderRoom>) {
-        binding.barrierFreeFacilitiesListView.adapter = RoomFinderListAdapter(this, response)
-        binding.barrierFreeFacilitiesListView.setOnItemClickListener { _, _, index, _ ->
+        listView.adapter = RoomFinderListAdapter(this, response)
+        listView.setOnItemClickListener { _, _, index, _ ->
             val facility = response[index]
             recents.insert(Recent(facility.toString(), RecentsDao.ROOMS))
             openRoomFinderDetails(facility)
