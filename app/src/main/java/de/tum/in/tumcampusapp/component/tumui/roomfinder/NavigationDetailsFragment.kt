ed555456775a24ee5dac6c2053e35ae84f345c06
ctsk <9384305+ctsk@users.noreply.github.com>
2022-11-17 18:26:32 +0100

    Location sharing: side by side buttons

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/roomfinder/NavigationDetailsFragment.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/roomfinder/NavigationDetailsFragment.kt
@@ -113,7 +113,7 @@ class NavigationDetailsFragment : BaseFragment<Unit>(
     private fun setupShareLocationButton(navigationDetails: NavigationDetails) {
         val url = "https://nav.tum.sexy/view/${navigationDetails.id}"
 
-        binding.toolbarNav.shareButton.setOnClickListener {
+        binding.shareLocationBtn.setOnClickListener {
             val sendIntent = Intent().apply {
                 action = Intent.ACTION_SEND
                 putExtra(Intent.EXTRA_TEXT, url)
