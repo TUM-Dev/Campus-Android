f358ca9a5ea8f7ce4c5787df5cbd2ee598bf4a91
ge78fug <ge78fug@mytum.de>
2022-12-01 11:34:20 +0100

    Fixed the recent search bug

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/search/SearchViewModel.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/search/SearchViewModel.kt
@@ -175,7 +175,7 @@ class SearchViewModel @Inject constructor(
         val recentSearchesDao: RecentsDao = TcaDb.getInstance(context).recentsDao()
         val recentSearches: List<Recent> = recentSearchesDao.allRecentSearches.reversed() ?: emptyList()
         state.value = state.value.copy(
-            recentSearches = recentSearches
+            recentSearches = recentSearches.filter { x -> x.type != 1 } // filter for undesirable stations
         )
     }
 
