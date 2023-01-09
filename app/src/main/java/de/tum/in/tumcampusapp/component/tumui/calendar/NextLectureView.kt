b3c555d9fc63f1e6b2583afeb21b9bb564b83bf8
Philipp <xarantolus@protonmail.com>
2023-01-09 21:47:33 +0100

    Next lecture card: show currently ongoing lectures (#1550)
    * Show current lecture in next lecture card
    
    * Partition events by first event day
    
    This hides events that are not on the same day behind the expand button
    
    * Only rerender same day lectures on changes
    
    * Make lecture time more human-readable
    
    This also shows the time for lectures that are tomorrow and handles
    currently ongoing lectures
    
    * Show remaining time for ongoing lecture
    
    * Make sure refresh shows correct lectures
    
    In case the lectures changed between refreshes, the wrong views were shown
    
    * Hide expansion on refresh
    
    This makes sure the expansion is hidden on refresh and also updates
    the button text
    
    * Fix linter error

--- app/src/main/java/de/tum/in/tumcampusapp/component/tumui/calendar/NextLectureView.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/tumui/calendar/NextLectureView.kt
@@ -1,6 +1,7 @@
 package de.tum.`in`.tumcampusapp.component.tumui.calendar
 
 import android.content.Context
+import android.text.format.DateUtils.*
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +9,13 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import de.tum.`in`.tumcampusapp.R
-import de.tum.`in`.tumcampusapp.utils.DateTimeUtils
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import org.joda.time.format.DateTimeFormat
+import java.util.*
 
-class NextLectureView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+class NextLectureView
+@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
 
     private val view = LayoutInflater.from(context).inflate(R.layout.layout_card_lecture, this, true)
 
@@ -24,7 +25,7 @@ class NextLectureView @JvmOverloads constructor(
         val lectureTitleTextView = view.findViewById<TextView>(R.id.lectureTitleTextView)
 
         lectureTitleTextView.text = lecture.title
-        lectureTimeTextView.text = DateTimeUtils.formatFutureTime(lecture.start, context)
+        lectureTimeTextView.text = formatLectureDate(lecture.start, lecture.end, context)
 
         if (lecture.locations == null || lecture.locations.isEmpty()) {
             lectureLocationTextView.visibility = View.GONE
@@ -33,14 +34,65 @@ class NextLectureView @JvmOverloads constructor(
             lectureLocationTextView.text = lecture.locationString
         }
 
-        view.setOnClickListener {
-            openEventBottomSheet(lecture)
+        view.setOnClickListener { openEventBottomSheet(lecture) }
+    }
+
+    /**
+     * Format a recently started or future date.
+     * Examples:
+     * - "1 h 20 m left"
+     * - "20 minutes left"
+     * - "Starts now"
+     * - "In 32 minutes"
+     * - "Today 18:30"
+     * - "Tomorrow 08:30"
+     * - "In 4 days"
+     */
+    private fun formatLectureDate(startTime: DateTime, endTime: DateTime, context: Context): String {
+        val timeInMillis = startTime.millis
+        val now = DateTime.now()
+
+        val diff = timeInMillis - now.millis
+
+        return when {
+            diff < 0 -> {
+                val timeToEnd = endTime.millis - now.millis
+                val formatter = DateTimeFormat.forPattern(if (timeToEnd < HOUR_IN_MILLIS) {
+                    "m 'min'"
+                } else {
+                    "h 'h' m 'min'"
+                }).withLocale(Locale.ENGLISH)
+                val readableTimeLeft = formatter.print(DateTime(timeToEnd, DateTimeZone.UTC))
+                context.getString(R.string.ongoing_until, readableTimeLeft)
+            }
+            diff < MINUTE_IN_MILLIS -> {
+                context.getString(R.string.starts_now)
+            }
+            diff < 5 * HOUR_IN_MILLIS -> {
+                val formatter = DateTimeFormat.forPattern(if (diff < HOUR_IN_MILLIS) {
+                    "m 'min'"
+                } else {
+                    "h 'h' m 'min'"
+                }).withLocale(Locale.ENGLISH)
+                "${context.getString(R.string.IN_capitalized)} ${formatter.print(DateTime(diff, DateTimeZone.UTC))}"
+            }
+            // Today
+            startTime.dayOfYear() == now.dayOfYear() && startTime.year() == now.year() -> {
+                DateTimeFormat.forPattern("HH:mm").withLocale(Locale.ENGLISH).print(startTime)
+            }
+            // Tomorrow
+            now.withDurationAdded(DAY_IN_MILLIS, 1).let { tomorrow ->
+                startTime.dayOfYear() == tomorrow.dayOfYear() && startTime.year() == tomorrow.year()
+            } -> {
+                val timeStr = DateTimeFormat.forPattern("HH:mm").withLocale(Locale.ENGLISH).print(startTime)
+                context.getString(R.string.tomorrow_time, timeStr)
+            }
+            else -> getRelativeTimeSpanString(timeInMillis, now.millis, MINUTE_IN_MILLIS, FORMAT_ABBREV_RELATIVE).toString()
         }
     }
 
     private fun openEventBottomSheet(item: NextLectureCard.CardCalendarItem) {
-        val detailsFragment =
-                CalendarDetailsFragment.newInstance(item.id, isShownInCalendarActivity = false)
+        val detailsFragment = CalendarDetailsFragment.newInstance(item.id, isShownInCalendarActivity = false)
         val activity = context as AppCompatActivity
         detailsFragment.show(activity.supportFragmentManager, null)
     }
