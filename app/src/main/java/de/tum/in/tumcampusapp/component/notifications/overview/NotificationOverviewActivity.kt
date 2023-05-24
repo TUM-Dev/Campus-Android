557b8fbe6a3a9e4bed284bac9c75a2185034ddc2
Franz <franz.kunze@tum.de>
2023-05-24 22:28:52 +0200

    Added get PendingIntent for every ScheduledNotification

--- app/src/main/java/de/tum/in/tumcampusapp/component/notifications/overview/NotificationOverviewActivity.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/notifications/overview/NotificationOverviewActivity.kt
@@ -5,13 +5,12 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.component.notifications.receivers.NotificationAlarmReceiver
+import de.tum.`in`.tumcampusapp.component.notifications.receivers.NotificationReceiver
 import de.tum.`in`.tumcampusapp.component.other.generic.activity.BaseActivity
 import de.tum.`in`.tumcampusapp.database.TcaDb
 import de.tum.`in`.tumcampusapp.databinding.ActivityNotificationOverviewBinding
-import de.tum.`in`.tumcampusapp.utils.Const
 
 class NotificationOverviewActivity : BaseActivity(R.layout.activity_notification_overview) {
 
@@ -34,51 +33,50 @@ class NotificationOverviewActivity : BaseActivity(R.layout.activity_notification
         // get notifications stored in database
         val scheduledNotifications = TcaDb.getInstance(applicationContext).scheduledNotificationsDao().getAllScheduledNotifications()
         val alarms = TcaDb.getInstance(applicationContext).activeNotificationsDao().getAllAlarms()
-        // get scheduled for AlarmManager per NotificationType by PendingIntent
+        // get PendingIntent for every NotificationType
         val typesList = (0..5)
+        // get PendingIntent for every ScheduledNotification
+        val scheduledNotificationIds = scheduledNotifications.map { it.typeId }
 
-        // TODO add alarm manager general scheduled
-
-        // TODO add alarm manager next
-
-
-        // TODO show settings of alarm manager?
-
-
-        // TODO remove
-        Toast.makeText(applicationContext, String.format("Number active: %d, Number scheduled: %d, Number alarms: %d",
-                activeNotifications.size, scheduledNotifications.size, alarms.size), Toast.LENGTH_LONG).show()
-
-
+        // store all notifications into one list
         val notificationsList = emptyList<NotificationItemForStickyList>().toMutableList()
-
-
         activeNotifications.forEach {
             notificationsList.add(NotificationItemForStickyList(it.toString(), getString(R.string.active_notifications)))
         }
-
         scheduledNotifications.forEach {
             notificationsList.add(NotificationItemForStickyList(it.toString(), getString(R.string.scheduled_notifications)))
         }
-
         alarms.forEach {
             notificationsList.add(NotificationItemForStickyList(it.toString(), getString(R.string.alarms)))
         }
-
         typesList.forEach { type ->
-            getAlarmIntent(type)?.let {
-                notificationsList.add(NotificationItemForStickyList(it.toString(), getString(R.string.alarmintents_per_type)))
+            getAlarmIntentPerType(type)?.let {
+                notificationsList.add(NotificationItemForStickyList(it.toString(), getString(R.string.pendingintents_per_type)))
+            }
+        }
+        scheduledNotificationIds.forEach { id ->
+            getAlarmIntent(id)?.let {
+                notificationsList.add(NotificationItemForStickyList(it.toString(), getString(R.string.pendingintents_per_scheduled_notification)))
             }
         }
 
         binding.notificationsListView.adapter = NotificationsListAdapter(applicationContext, notificationsList)
     }
 
-    private fun getAlarmIntent(type: Int): PendingIntent? {
-        val intent = Intent(applicationContext, NotificationAlarmReceiver::class.java).apply {
-            putExtra(Const.KEY_NOTIFICATION_TYPE_ID, type)
-        }
+    // mimics NotificationScheduler.getAlarmIntent(type: NotificationType) method
+    private fun getAlarmIntentPerType(type: Int): PendingIntent? {
+        // extra data is ignored according to intent filter
+        val intent = Intent(applicationContext, NotificationAlarmReceiver::class.java)
         // FLAG_NO_CREATE to only show existing intents
         return PendingIntent.getBroadcast(applicationContext, type, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_NO_CREATE)
+    }
+
+    // mimics NotificationScheduler.getAlarmIntent(futureNotification: FutureNotification, globalNotificationId: Long) method
+    private fun getAlarmIntent(id: Int): PendingIntent? {
+        // extra data is ignored according to intent filter
+        val intent = Intent(applicationContext, NotificationReceiver::class.java)
+        // FLAG_NO_CREATE to only show existing intents
+        return PendingIntent.getBroadcast(applicationContext,
+                id, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_NO_CREATE)
     }
 }
