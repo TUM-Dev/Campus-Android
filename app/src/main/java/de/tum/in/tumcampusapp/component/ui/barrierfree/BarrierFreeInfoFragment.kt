006972c1139eddf808efbce9bfab06223574b590
Frank Elsinga <frank@elsinga.de>
2023-09-10 13:43:43 +0200

    removed the need for `BarrierFreeFacilitiesActivity`

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/barrierfree/BarrierFreeInfoFragment.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/barrierfree/BarrierFreeInfoFragment.kt
@@ -1,11 +1,15 @@
 package de.tum.`in`.tumcampusapp.component.ui.barrierfree
 
+import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.component.other.generic.fragment.BaseFragment
+import de.tum.`in`.tumcampusapp.component.tumui.roomfinder.NavigationDetailsFragment
+import de.tum.`in`.tumcampusapp.component.ui.search.SearchActivity
+import de.tum.`in`.tumcampusapp.component.ui.search.SearchFragment
 import de.tum.`in`.tumcampusapp.databinding.FragmentBarrierfreeInfoBinding
 
 class BarrierFreeInfoFragment : BaseFragment<Unit>(
@@ -22,7 +26,13 @@ class BarrierFreeInfoFragment : BaseFragment<Unit>(
         binding.barrierFreeListView.setOnItemClickListener { _, _, position, _ ->
             val intent = when (position) {
                 0 -> Intent(requireContext(), BarrierFreeContactActivity::class.java)
-                1 -> Intent(requireContext(), BarrierFreeFacilitiesActivity::class.java)
+                1 -> {
+                    val tmp_int=Intent(requireContext(), SearchActivity::class.java)
+                    val lat =0.0
+                    val lon =0.0
+                    tmp_int.putExtra(SearchManager.QUERY, "type:room near:${lat},${lon}")
+                    tmp_int
+                }
                 2 -> Intent(requireContext(), BarrierFreeMoreInfoActivity::class.java)
                 else -> throw IllegalStateException("Invalid index $position in BarrierFreeInfoFragment")
             }
