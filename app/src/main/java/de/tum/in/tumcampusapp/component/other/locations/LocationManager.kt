2d5189f95aff99fa6bf802deda8ec3b8e44cefbb
Frank Elsinga <frank@elsinga.de>
2023-09-16 19:48:55 +0200

    Update app/src/main/java/de/tum/in/tumcampusapp/component/other/locations/LocationManager.kt

--- app/src/main/java/de/tum/in/tumcampusapp/component/other/locations/LocationManager.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/other/locations/LocationManager.kt
@@ -417,10 +417,7 @@ class LocationManager @Inject constructor(c: Context) {
             val d7 = d3 * cos(d14) * cos(d14)
             val d8 = (d1 * (1 - d2)) / (1 - d2 * sin(d14) * sin(d14)).pow(1.5)
             val d9 = d15 / (d5 * d)
-            var d17 =
-                d14 - ((d5 * tan(d14)) / d8) * ((d9 * d9) / 2 - ((5 + 3 * d6 + 10 * d7 - 4 * d7 * d7 - 9 * d3) * d9.pow(4.0)) / 24 + ((61 + 90 * d6 + 298 * d7 + 45 * d6 * d6 - 252 * d3 - 3 * d7 * d7) * d9.pow(
-                    6.0
-                )) / 720)
+            var d17 = d14 - ((d5 * tan(d14)) / d8) * ((d9 * d9) / 2 - ((5 + 3 * d6 + 10 * d7 - 4 * d7 * d7 - 9 * d3) * d9.pow(4.0)) / 24 + ((61 + 90 * d6 + 298 * d7 + 45 * d6 * d6 - 252 * d3 - 3 * d7 * d7) * d9.pow(6.0)) / 720)
             d17 *= 180 / Math.PI
             var d18 =
                 (d9 - ((1 + 2 * d6 + d7) * d9.pow(3.0)) / 6 + ((5 - 2 * d7 + 28 * d6 - 3 * d7 * d7 + 8 * d3 + 24 * d6 * d6) * d9.pow(5.0)) / 120) / cos(
